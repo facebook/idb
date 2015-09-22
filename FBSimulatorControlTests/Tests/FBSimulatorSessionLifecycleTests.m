668f0a26fd210dc54c4bbc1a529f339cc45b0a68
Lawrence Lomax <lawrencelomax@fb.com>
2015-09-22 13:24:39 +0100

    Expected Notification assertion

--- FBSimulatorControlTests/Tests/FBSimulatorSessionLifecycleTests.m
+++ FBSimulatorControlTests/Tests/FBSimulatorSessionLifecycleTests.m
@@ -94,6 +94,60 @@
   XCTAssertTrue(didMeetConditionBeforeTimeout);
   XCTAssertTrue(wasUnexpected);
   [NSNotificationCenter.defaultCenter removeObserver:token];
+
+  XCTAssertFalse([session.state processForApplication:appLaunch.application]);
 }
+
+- (void)testNotifiedByExpectedApplicationTermination
+{
+  NSError *error = nil;
+  FBSimulatorSession *session = [self.control createSessionForSimulatorConfiguration:FBSimulatorConfiguration.iPhone5 error:&error];
+
+  FBApplicationLaunchConfiguration *appLaunch = [FBApplicationLaunchConfiguration
+    configurationWithApplication:[FBSimulatorApplication systemApplicationNamed:@"MobileSafari"]
+    arguments:@[]
+    environment:@{}];
+
+  BOOL success = [[[session.interact
+    bootSimulator]
+    launchApplication:appLaunch]
+    performInteractionWithError:&error];
+
+  XCTAssertTrue(success);
+  XCTAssertNil(error);
+
+  FBUserLaunchedProcess *process = [session.state processForApplication:appLaunch.application];
+  XCTAssertNotNil(process);
+  if (!process) {
+    // Need to guard against continuing the test in case the PID is 0 or -1 to avoid nuking the machine.
+    return;
+  }
+
+  __block BOOL notificationRecieved = NO;
+  __block BOOL wasExpected = NO;
+
+  id token = [NSNotificationCenter.defaultCenter
+    addObserverForName:FBSimulatorSessionApplicationProcessDidTerminateNotification
+    object:session
+    queue:NSOperationQueue.mainQueue
+    usingBlock:^(NSNotification *notification) {
+      notificationRecieved = YES;
+      wasExpected = [notification.userInfo[FBSimulatorSessionExpectedKey] boolValue];
+    }];
+
+  XCTAssertTrue([[session.interact killApplication:appLaunch.application] performInteractionWithError:&error]);
+  XCTAssertNil(error);
+
+  BOOL didMeetConditionBeforeTimeout = [NSRunLoop.currentRunLoop spinRunLoopWithTimeout:10 untilTrue:^ BOOL {
+    return notificationRecieved == YES;
+  }];
+
+  XCTAssertTrue(didMeetConditionBeforeTimeout);
+  XCTAssertTrue(wasExpected);
+  [NSNotificationCenter.defaultCenter removeObserver:token];
+
+  XCTAssertFalse([session.state processForApplication:appLaunch.application]);
+}
+
 
 @end
