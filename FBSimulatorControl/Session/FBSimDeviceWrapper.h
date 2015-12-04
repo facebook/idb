2b6fe954ce60c43989eacf11271316ede6937618
lawrencelomax <lawrencelomax@fb.com>
2015-12-04 11:05:26 +0000

    Get Process Info on Process Launch

--- FBSimulatorControl/Session/FBSimDeviceWrapper.h
+++ FBSimulatorControl/Session/FBSimDeviceWrapper.h
@@ -9,13 +9,53 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol FBProcessInfo;
 @class SimDevice;
+@class FBProcessQuery;
 
+/**
+ Augments SimDevice.
+ */
 @interface FBSimDeviceWrapper : NSObject
 
-- (instancetype)initWithSimDevice:(SimDevice *)device;
+/**
+ Creates a SimDevice Wrapper.
 
-- (int)launchApplicationWithID:(NSString *)appID options:(NSDictionary *)options error:(NSError **)error;
+ @param device the device to wrap
+ @param processQuery the Process Query to obtain process information.
+ @return a new SimDevice wrapper.
+ */
++ (instancetype)withSimDevice:(SimDevice *)device processQuery:(FBProcessQuery *)processQuery;
+
+/**
+ Boots an Application, timing out if CoreSimulator gets stuck in a semaphore.
+
+ @param appID the Application ID to use.
+ @param options the Options to use in the launch.
+ @param error an error out for any error that occured.
+ @return the Process Identifier of the launched process, -1 otherwise.
+ */
+- (id<FBProcessInfo>)launchApplicationWithID:(NSString *)appID options:(NSDictionary *)options error:(NSError **)error;
+
+/**
+ Installs an Application, timing out if CoreSimulator gets stuck in a semaphore.
+
+ @param appURL the Application URL to use.
+ @param options the Options to use in the launch.
+ @param error an error out for any error that occured.
+ @return YES if the Application was installed successfully, NO otherwise.
+ */
 - (BOOL)installApplication:(NSURL *)appURL withOptions:(NSDictionary *)options error:(NSError **)error;
+
+/**
+ Spawns a binary, timing out if CoreSimulator gets stuck in a semaphore.
+
+ @param launchPath the path to the binary.
+ @param options the Options to use in the launch.
+ @param terminationHandler ?????
+ @param error an error out for any error that occured.
+ @return the Process Identifier of the launched process, -1 otherwise.
+ */
+- (id<FBProcessInfo>)spawnWithPath:(NSString *)launchPath options:(NSDictionary *)options terminationHandler:(id)terminationHandler error:(NSError **)error;
 
 @end
--- FBSimulatorControl/Session/FBSimDeviceWrapper.m
+++ FBSimulatorControl/Session/FBSimDeviceWrapper.m
@@ -11,25 +11,35 @@
 
 #import <CoreSimulator/SimDevice.h>
 
+#import "FBProcessInfo.h"
+#import "FBProcessQuery.h"
 #import "FBSimulatorError.h"
+#import "NSRunLoop+SimulatorControlAdditions.h"
 
 const long kFBSimDeviceCommandTimeout = 30;
 
 @interface FBSimDeviceWrapper ()
 
-@property (atomic, assign) SimDevice *device;
+@property (nonatomic, strong, readonly) SimDevice *device;
+@property (nonatomic, strong, readonly) FBProcessQuery *query;
 
 @end
 
 @implementation FBSimDeviceWrapper
 
-- (instancetype)initWithSimDevice:(SimDevice *)device
++ (instancetype)withSimDevice:(SimDevice *)device processQuery:(FBProcessQuery *)processQuery
+{
+  return [[self alloc] initWithSimDevice:device processQuery:processQuery];
+}
+
+- (instancetype)initWithSimDevice:(SimDevice *)device processQuery:(FBProcessQuery *)query
 {
   if (!(self = [self init])) {
     return nil;
   }
 
   _device = device;
+  _query = query;
 
   return self;
 }
@@ -56,7 +66,7 @@ const long kFBSimDeviceCommandTimeout = 30;
   dispatch_semaphore_signal(semaphore);
 }
 
-- (int)launchApplicationWithID:(NSString *)appID options:(NSDictionary *)options error:(NSError **)error
+- (id<FBProcessInfo>)launchApplicationWithID:(NSString *)appID options:(NSDictionary *)options error:(NSError **)error
 {
   NSAssert([NSThread isMainThread], @"Must be called from the main thread.");
 
@@ -68,13 +78,12 @@ const long kFBSimDeviceCommandTimeout = 30;
   [invocation setArgument:&error atIndex:4];
 
   if (![self runInvocationInBackgroundUntilTimeout:invocation]) {
-    [[FBSimulatorError describe:@"Timed out calling launchApplicationWithID"] fail:error];
-    return 0;
+    return [[FBSimulatorError describe:@"Timed out calling launchApplicationWithID"] fail:error];
   }
 
   pid_t pid;
   [invocation getReturnValue:&pid];
-  return pid;
+  return [self processInfoForProcessIdentifier:pid error:error];
 }
 
 - (BOOL)installApplication:(NSURL *)appURL withOptions:(NSDictionary *)options error:(NSError **)error
@@ -89,13 +98,35 @@ const long kFBSimDeviceCommandTimeout = 30;
   [invocation setArgument:&error atIndex:4];
 
   if (![self runInvocationInBackgroundUntilTimeout:invocation]) {
-    [[FBSimulatorError describe:@"Timed out calling installApplication"] fail:error];
-    return NO;
+    return [[FBSimulatorError describe:@"Timed out calling installApplication"] failBool:error];
   }
 
   BOOL rv;
   [invocation getReturnValue:&rv];
   return rv;
+}
+
+- (id<FBProcessInfo>)spawnWithPath:(NSString *)launchPath options:(NSDictionary *)options terminationHandler:(id)terminationHandler error:(NSError **)error
+{
+  pid_t processIdentifier = [self.device spawnWithPath:launchPath options:options terminationHandler:terminationHandler error:error];
+  return [self processInfoForProcessIdentifier:processIdentifier error:error];
+}
+
+#pragma mark Private
+
+- (id<FBProcessInfo>)processInfoForProcessIdentifier:(pid_t)processIdentifier error:(NSError **)error
+{
+  if (processIdentifier <= -1) {
+    return nil;
+  }
+
+  id<FBProcessInfo> processInfo = [NSRunLoop.currentRunLoop spinRunLoopWithTimeout:5 untilExists:^ id<FBProcessInfo> {
+    return [self.query processInfoFor:processIdentifier];
+  }];
+  if (!processInfo) {
+    return [[FBSimulatorError describeFormat:@"Timed out waiting for process info for pid %d", processIdentifier] fail:error];
+  }
+  return processInfo;
 }
 
 @end
