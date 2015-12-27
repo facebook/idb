lifeiheng/FBSimulatorControl

Add Log Levels with additional error & debug logging

FBSimulatorControl/Management/FBSimulatorTerminationStrategy.m
@@ -14,6 +14,7 @@
 #import <CoreSimulator/SimDeviceType.h>
 #import <CoreSimulator/SimRuntime.h>
 
+#import "FBCollectionDescriptions.h"
 #import "FBCoreSimulatorNotifier.h"
 #import "FBProcessInfo.h"
 #import "FBProcessQuery+Simulators.h"
@@ -37,6 +38,7 @@
 
 @property (nonatomic, copy, readonly) FBSimulatorControlConfiguration *configuration;
 @property (nonatomic, strong, readonly) FBProcessQuery *processQuery;
+@property (nonatomic, strong, readonly) id<FBSimulatorLogger> logger;
 
 - (BOOL)killSimulatorProcess:(FBProcessInfo *)process error:(NSError **)error;
 - (BOOL)killProcesses:(NSArray *)processes error:(NSError **)error;
@@ -86,9 +88,10 @@
   if (application.isTerminated) {
     return YES;
   }
-  return [[[FBSimulatorError
+  return [[[[FBSimulatorError
     describeFormat:@"Could not terminate Application %@", application]
     attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+    logger:self.logger]
     failBool:error];
 }
 
@@ -98,16 +101,16 @@
 
 #pragma mark Initialization
 
-+ (instancetype)withConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery
++ (instancetype)withConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery logger:(id<FBSimulatorLogger>)logger
 {
   BOOL useKill = (configuration.options & FBSimulatorManagementOptionsUseProcessKilling) == FBSimulatorManagementOptionsUseProcessKilling;
   processQuery = processQuery ?: [FBProcessQuery new];
   return useKill
-    ? [[FBSimulatorTerminationStrategy_Kill alloc] initWithConfiguration:configuration processQuery:processQuery]
-    : [[FBSimulatorTerminationStrategy_WorkspaceQuit alloc] initWithConfiguration:configuration processQuery:processQuery];
+    ? [[FBSimulatorTerminationStrategy_Kill alloc] initWithConfiguration:configuration processQuery:processQuery logger:logger]
+    : [[FBSimulatorTerminationStrategy_WorkspaceQuit alloc] initWithConfiguration:configuration processQuery:processQuery logger:logger];
 }
 
-- (instancetype)initWithConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery
+- (instancetype)initWithConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery logger:(id<FBSimulatorLogger>)logger
 {
   self = [super init];
   if (!self) {
@@ -116,6 +119,7 @@
 
   _configuration = configuration;
   _processQuery = processQuery;
+  _logger = logger;
 
   return self;
 }
@@ -137,18 +141,31 @@
   // Since `safeShutdown:withError:` will spin the run loop until CoreSimulator confirms that the device is shutdown,
   // this will give a sufficient amount of time between killing Applications.
 
+  [self.logger.debug log:@"Killing %@", [FBCollectionDescriptions oneLineDescriptionFromArray:simulators atKeyPath:@"shortDescription"]];
   for (FBSimulator *simulator in simulators) {
     FBProcessInfo *simulatorProcess = simulator.launchInfo.simulatorProcess ?: [self.processQuery simulatorApplicationProcessForSimDevice:simulator.device];
     if (!simulatorProcess) {
+      [self.logger.debug log:@"Will not kill %@ as it is not launched", simulator.shortDescription];
       continue;
     }
     NSError *innerError = nil;
     if (![self killSimulatorProcess:simulatorProcess error:&innerError]) {
-      return [[[[FBSimulatorError describeFormat:@"Could not kill simulator process %@", simulatorProcess] inSimulator:simulator] causedBy:innerError] fail:error];
+      return [[[[[FBSimulatorError
+        describeFormat:@"Could not kill simulator process %@", simulatorProcess]
+        inSimulator:simulator]
+        causedBy:innerError]
+        logger:self.logger]
+        fail:error];
     }
     if (![self safeShutdownSimulator:simulator withError:&innerError]) {
-      return [[[[FBSimulatorError describe:@"Could not shut down simulator after termination"] inSimulator:simulator] causedBy:innerError] fail:error];
+      return [[[[[FBSimulatorError
+        describe:@"Could not shut down simulator after termination"]
+        inSimulator:simulator]
+        causedBy:innerError]
+        logger:self.logger]
+        fail:error];
     }
+    [self.logger.info log:@"Killed & Shutdown %@", simulator];
   }
   return simulators;
 }
@@ -173,17 +190,21 @@
     if (![simulator waitOnState:FBSimulatorStateShutdown withError:&innerError]) {
       // In Xcode 7 we can get stuck in the 'Creating' step as well, its possible that we can recover from this by erasing
       if (![simulator eraseWithError:&innerError]) {
-        return [[[[FBSimulatorError describe:@"Failed trying to prepare simulator for usage by erasing a stuck 'Creating' simulator %@"]
+        return [[[[[FBSimulatorError
+          describe:@"Failed trying to prepare simulator for usage by erasing a stuck 'Creating' simulator %@"]
           causedBy:innerError]
           inSimulator:simulator]
+          logger:self.logger]
           failBool:error];
       }
 
       // If a device has been erased, we should wait for it to actually be shutdown. Ff it can't be, fail
       if (![simulator waitOnState:FBSimulatorStateShutdown withError:&innerError]) {
-        return [[[[FBSimulatorError describe:@"Failed trying to wait for a 'Creating' simulator to be shutdown after being erased"]
+        return [[[[[FBSimulatorError
+          describe:@"Failed trying to wait for a 'Creating' simulator to be shutdown after being erased"]
           causedBy:innerError]
           inSimulator:simulator]
+          logger:self.logger]
           failBool:error];
       }
     }
@@ -195,12 +216,22 @@
   // Code 159 (Xcode 7) or 146 (Xcode 6) is 'Unable to shutdown device in current state: Shutdown'
   // We can safely ignore these codes and then confirm that the simulator is truly shutdown.
   if (![simulator.device shutdownWithError:&innerError] && innerError.code != 159 && innerError.code != 146) {
-    return [FBSimulatorError failBoolWithError:innerError description:@"Simulator could not be shutdown" errorOut:error];
+    return [[[[[FBSimulatorError
+      describe:@"Simulator could not be shutdown"]
+      causedBy:innerError]
+      inSimulator:simulator]
+      logger:self.logger]
+      failBool:error];
   }
 
   // Wait for it to be truly shutdown.
   if (![simulator waitOnState:FBSimulatorStateShutdown withError:&innerError]) {
-    return [[[[FBSimulatorError describe:@"Failed to wait for simulator preparation to shutdown device"] causedBy:innerError] inSimulator:simulator] failBool:error];
+    return [[[[[FBSimulatorError
+      describe:@"Failed to wait for simulator preparation to shutdown device"]
+      causedBy:innerError]
+      inSimulator:simulator]
+      logger:self.logger]
+      failBool:error];
   }
   return YES;
 }
@@ -215,7 +246,11 @@
   for (FBSimulator *simulator in simulators) {
     NSError *innerError = nil;
     if (![self safeShutdownSimulator:simulator withError:&innerError]) {
-      return [[[FBSimulatorError describe:@"Failed to ensure consistency by shutting down process-les simulators"] inSimulator:simulator] fail:error];
+      return [[[[FBSimulatorError
+        describe:@"Failed to ensure consistency by shutting down process-less simulators"]
+        inSimulator:simulator]
+        logger:self.logger]
+        fail:error];
     }
   }
   return simulators;
@@ -232,7 +267,11 @@
 
   NSError *innerError = nil;
   if (![self killSimulatorProcessesMatchingPredicate:predicate error:&innerError]) {
-    return [[[FBSimulatorError describe:@"Could not kill non-current spurious simulators"] causedBy:innerError] failBool:error];
+    return [[[[FBSimulatorError
+      describe:@"Could not kill spurious simulators"]
+      causedBy:innerError]
+      logger:self.logger]
+      failBool:error];
   }
 
   return YES;
@@ -253,28 +292,33 @@
 - (BOOL)killProcess:(FBProcessInfo *)process error:(NSError **)error
 {
   // The kill was successful, all is well.
+  [self.logger.debug log:@"Killing %@", process.shortDescription];
   if (kill(process.processIdentifier, SIGTERM) == 0) {
     return YES;
   }
   int errorCode = errno;
   if (errorCode == EPERM) {
-    return [[[FBSimulatorError
+    return [[[[FBSimulatorError
       describeFormat:@"Failed to kill process %@ as the sending process does not have the privelages", process]
       attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+      logger:self.logger]
       failBool:error];
   }
   if (errorCode == ESRCH) {
-    return [[[FBSimulatorError
+    return [[[[FBSimulatorError
       describeFormat:@"Failed to kill process %@ as the sending process does not exist", process]
       attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+      logger:self.logger]
       failBool:error];
   }
   if (errorCode == EINVAL) {
-    return [[[FBSimulatorError
+    return [[[[FBSimulatorError
       describeFormat:@"Failed to kill process %@ as the signal was not a valid signal number", process]
       attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+      logger:self.logger]
       failBool:error];
   }
+  [self.logger.debug log:@"Killed %@", process.shortDescription];
   return [[FBSimulatorError describeFormat:@"Failed to kill process %@ with unknown errno %d", process, errorCode] failBool:error];
 }
 

FBSimulatorControl/Utility/FBSimulatorError.m
@@ -22,6 +22,7 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
 
 @property (nonatomic, copy, readwrite) NSString *describedAs;
 @property (nonatomic, copy, readwrite) NSError *cause;
+@property (nonatomic, strong, readwrite) id<FBSimulatorLogger> logger;
 @property (nonatomic, strong, readwrite) NSMutableDictionary *additionalInfo;
 @property (nonatomic, assign, readwrite) BOOL describeRecursively;
 
@@ -38,6 +39,8 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
 
   _additionalInfo = [NSMutableDictionary dictionary];
   _describeRecursively = YES;
+  _logger = FBSimulatorControlStaticConfiguration.defaultLogger;
+
   return self;
 }
 
@@ -140,6 +143,12 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
     value:[query processInfoFor:processIdentifier] ?: @"No Process Info"];
 }
 
+- (instancetype)logger:(id<FBSimulatorLogger>)logger
+{
+  self.logger = logger;
+  return self;
+}
+
 - (NSError *)build
 {
   // If there's just a cause, there's no error to build
@@ -155,8 +164,9 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
     userInfo[NSUnderlyingErrorKey] = self.underlyingError;
   }
   [userInfo addEntriesFromDictionary:self.additionalInfo];
+
   NSError *error = [NSError errorWithDomain:FBSimulatorControlErrorDomain code:0 userInfo:[userInfo copy]];
-  [FBSimulatorControlStaticConfiguration.defaultLogger logMessage:@"Error => %@", error];
+  [self.logger.error log:@"Error => %@", error];
   return error;
 }
 

facebook/idb

FBSimulatorControl/Management/FBSimulatorTerminationStrategy.m
@@ -14,6 +14,7 @@
 #import <CoreSimulator/SimDeviceType.h>
 #import <CoreSimulator/SimRuntime.h>
 
+#import "FBCollectionDescriptions.h"
 #import "FBCoreSimulatorNotifier.h"
 #import "FBProcessInfo.h"
 #import "FBProcessQuery+Simulators.h"
@@ -37,6 +38,7 @@
 
 @property (nonatomic, copy, readonly) FBSimulatorControlConfiguration *configuration;
 @property (nonatomic, strong, readonly) FBProcessQuery *processQuery;
+@property (nonatomic, strong, readonly) id<FBSimulatorLogger> logger;
 
 - (BOOL)killSimulatorProcess:(FBProcessInfo *)process error:(NSError **)error;
 - (BOOL)killProcesses:(NSArray *)processes error:(NSError **)error;
@@ -86,9 +88,10 @@
   if (application.isTerminated) {
     return YES;
   }
-  return [[[FBSimulatorError
+  return [[[[FBSimulatorError
     describeFormat:@"Could not terminate Application %@", application]
     attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+    logger:self.logger]
     failBool:error];
 }
 
@@ -98,16 +101,16 @@
 
 #pragma mark Initialization
 
-+ (instancetype)withConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery
++ (instancetype)withConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery logger:(id<FBSimulatorLogger>)logger
 {
   BOOL useKill = (configuration.options & FBSimulatorManagementOptionsUseProcessKilling) == FBSimulatorManagementOptionsUseProcessKilling;
   processQuery = processQuery ?: [FBProcessQuery new];
   return useKill
-    ? [[FBSimulatorTerminationStrategy_Kill alloc] initWithConfiguration:configuration processQuery:processQuery]
-    : [[FBSimulatorTerminationStrategy_WorkspaceQuit alloc] initWithConfiguration:configuration processQuery:processQuery];
+    ? [[FBSimulatorTerminationStrategy_Kill alloc] initWithConfiguration:configuration processQuery:processQuery logger:logger]
+    : [[FBSimulatorTerminationStrategy_WorkspaceQuit alloc] initWithConfiguration:configuration processQuery:processQuery logger:logger];
 }
 
-- (instancetype)initWithConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery
+- (instancetype)initWithConfiguration:(FBSimulatorControlConfiguration *)configuration processQuery:(FBProcessQuery *)processQuery logger:(id<FBSimulatorLogger>)logger
 {
   self = [super init];
   if (!self) {
@@ -116,6 +119,7 @@
 
   _configuration = configuration;
   _processQuery = processQuery;
+  _logger = logger;
 
   return self;
 }
@@ -137,18 +141,31 @@
   // Since `safeShutdown:withError:` will spin the run loop until CoreSimulator confirms that the device is shutdown,
   // this will give a sufficient amount of time between killing Applications.
 
+  [self.logger.debug logFormat:@"Killing %@", [FBCollectionDescriptions oneLineDescriptionFromArray:simulators atKeyPath:@"shortDescription"]];
   for (FBSimulator *simulator in simulators) {
     FBProcessInfo *simulatorProcess = simulator.launchInfo.simulatorProcess ?: [self.processQuery simulatorApplicationProcessForSimDevice:simulator.device];
     if (!simulatorProcess) {
+      [self.logger.debug logFormat:@"Will not kill %@ as it is not launched", simulator.shortDescription];
       continue;
     }
     NSError *innerError = nil;
     if (![self killSimulatorProcess:simulatorProcess error:&innerError]) {
-      return [[[[FBSimulatorError describeFormat:@"Could not kill simulator process %@", simulatorProcess] inSimulator:simulator] causedBy:innerError] fail:error];
+      return [[[[[FBSimulatorError
+        describeFormat:@"Could not kill simulator process %@", simulatorProcess]
+        inSimulator:simulator]
+        causedBy:innerError]
+        logger:self.logger]
+        fail:error];
     }
     if (![self safeShutdownSimulator:simulator withError:&innerError]) {
-      return [[[[FBSimulatorError describe:@"Could not shut down simulator after termination"] inSimulator:simulator] causedBy:innerError] fail:error];
+      return [[[[[FBSimulatorError
+        describe:@"Could not shut down simulator after termination"]
+        inSimulator:simulator]
+        causedBy:innerError]
+        logger:self.logger]
+        fail:error];
     }
+    [self.logger.info logFormat:@"Killed & Shutdown %@", simulator];
   }
   return simulators;
 }
@@ -173,17 +190,21 @@
     if (![simulator waitOnState:FBSimulatorStateShutdown withError:&innerError]) {
       // In Xcode 7 we can get stuck in the 'Creating' step as well, its possible that we can recover from this by erasing
       if (![simulator eraseWithError:&innerError]) {
-        return [[[[FBSimulatorError describe:@"Failed trying to prepare simulator for usage by erasing a stuck 'Creating' simulator %@"]
+        return [[[[[FBSimulatorError
+          describe:@"Failed trying to prepare simulator for usage by erasing a stuck 'Creating' simulator %@"]
           causedBy:innerError]
           inSimulator:simulator]
+          logger:self.logger]
           failBool:error];
       }
 
       // If a device has been erased, we should wait for it to actually be shutdown. Ff it can't be, fail
       if (![simulator waitOnState:FBSimulatorStateShutdown withError:&innerError]) {
-        return [[[[FBSimulatorError describe:@"Failed trying to wait for a 'Creating' simulator to be shutdown after being erased"]
+        return [[[[[FBSimulatorError
+          describe:@"Failed trying to wait for a 'Creating' simulator to be shutdown after being erased"]
           causedBy:innerError]
           inSimulator:simulator]
+          logger:self.logger]
           failBool:error];
       }
     }
@@ -195,12 +216,22 @@
   // Code 159 (Xcode 7) or 146 (Xcode 6) is 'Unable to shutdown device in current state: Shutdown'
   // We can safely ignore these codes and then confirm that the simulator is truly shutdown.
   if (![simulator.device shutdownWithError:&innerError] && innerError.code != 159 && innerError.code != 146) {
-    return [FBSimulatorError failBoolWithError:innerError description:@"Simulator could not be shutdown" errorOut:error];
+    return [[[[[FBSimulatorError
+      describe:@"Simulator could not be shutdown"]
+      causedBy:innerError]
+      inSimulator:simulator]
+      logger:self.logger]
+      failBool:error];
   }
 
   // Wait for it to be truly shutdown.
   if (![simulator waitOnState:FBSimulatorStateShutdown withError:&innerError]) {
-    return [[[[FBSimulatorError describe:@"Failed to wait for simulator preparation to shutdown device"] causedBy:innerError] inSimulator:simulator] failBool:error];
+    return [[[[[FBSimulatorError
+      describe:@"Failed to wait for simulator preparation to shutdown device"]
+      causedBy:innerError]
+      inSimulator:simulator]
+      logger:self.logger]
+      failBool:error];
   }
   return YES;
 }
@@ -215,7 +246,11 @@
   for (FBSimulator *simulator in simulators) {
     NSError *innerError = nil;
     if (![self safeShutdownSimulator:simulator withError:&innerError]) {
-      return [[[FBSimulatorError describe:@"Failed to ensure consistency by shutting down process-les simulators"] inSimulator:simulator] fail:error];
+      return [[[[FBSimulatorError
+        describe:@"Failed to ensure consistency by shutting down process-less simulators"]
+        inSimulator:simulator]
+        logger:self.logger]
+        fail:error];
     }
   }
   return simulators;
@@ -232,7 +267,11 @@
 
   NSError *innerError = nil;
   if (![self killSimulatorProcessesMatchingPredicate:predicate error:&innerError]) {
-    return [[[FBSimulatorError describe:@"Could not kill non-current spurious simulators"] causedBy:innerError] failBool:error];
+    return [[[[FBSimulatorError
+      describe:@"Could not kill spurious simulators"]
+      causedBy:innerError]
+      logger:self.logger]
+      failBool:error];
   }
 
   return YES;
@@ -253,28 +292,33 @@
 - (BOOL)killProcess:(FBProcessInfo *)process error:(NSError **)error
 {
   // The kill was successful, all is well.
+  [self.logger.debug logFormat:@"Killing %@", process.shortDescription];
   if (kill(process.processIdentifier, SIGTERM) == 0) {
     return YES;
   }
   int errorCode = errno;
   if (errorCode == EPERM) {
-    return [[[FBSimulatorError
+    return [[[[FBSimulatorError
       describeFormat:@"Failed to kill process %@ as the sending process does not have the privelages", process]
       attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+      logger:self.logger]
       failBool:error];
   }
   if (errorCode == ESRCH) {
-    return [[[FBSimulatorError
+    return [[[[FBSimulatorError
       describeFormat:@"Failed to kill process %@ as the sending process does not exist", process]
       attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+      logger:self.logger]
       failBool:error];
   }
   if (errorCode == EINVAL) {
-    return [[[FBSimulatorError
+    return [[[[FBSimulatorError
       describeFormat:@"Failed to kill process %@ as the signal was not a valid signal number", process]
       attachProcessInfoForIdentifier:process.processIdentifier query:self.processQuery]
+      logger:self.logger]
       failBool:error];
   }
+  [self.logger.debug logFormat:@"Killed %@", process.shortDescription];
   return [[FBSimulatorError describeFormat:@"Failed to kill process %@ with unknown errno %d", process, errorCode] failBool:error];
 }
 

FBSimulatorControl/Utility/FBSimulatorError.m
@@ -23,6 +23,7 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
 
 @property (nonatomic, copy, readwrite) NSString *describedAs;
 @property (nonatomic, copy, readwrite) NSError *cause;
+@property (nonatomic, strong, readwrite) id<FBSimulatorLogger> logger;
 @property (nonatomic, strong, readwrite) NSMutableDictionary *additionalInfo;
 @property (nonatomic, assign, readwrite) BOOL describeRecursively;
 
@@ -39,6 +40,8 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
 
   _additionalInfo = [NSMutableDictionary dictionary];
   _describeRecursively = YES;
+  _logger = FBSimulatorControlStaticConfiguration.defaultLogger;
+
   return self;
 }
 
@@ -141,6 +144,12 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
     value:[query processInfoFor:processIdentifier] ?: @"No Process Info"];
 }
 
+- (instancetype)logger:(id<FBSimulatorLogger>)logger
+{
+  self.logger = logger;
+  return self;
+}
+
 - (NSError *)build
 {
   // If there's just a cause, there's no error to build
@@ -156,8 +165,9 @@ NSString *const FBSimulatorControlErrorDomain = @"com.facebook.FBSimulatorContro
     userInfo[NSUnderlyingErrorKey] = self.underlyingError;
   }
   [userInfo addEntriesFromDictionary:self.additionalInfo];
+
   NSError *error = [NSError errorWithDomain:FBSimulatorControlErrorDomain code:0 userInfo:[userInfo copy]];
-  [FBSimulatorControlStaticConfiguration.defaultLogger logMessage:@"Error => %@", error];
+  [self.logger.error logFormat:@"Error => %@", error];
   return error;
 }
 