f0b93e6f0fb9495fd150f31f6e331db40666be48
lawrencelomax <lawrencelomax@fb.com>
2016-01-22 08:47:20 +0000

    Add JSONEventReporter

--- fbsimctl/FBSimulatorControlKit/Sources/EventReporter.swift
+++ fbsimctl/FBSimulatorControlKit/Sources/EventReporter.swift
@@ -105,3 +105,82 @@ public class HumanReadableEventReporter : NSObject, EventReporter {
     }
   }
 }
+
+public class JSONEventReporter : NSObject, EventReporter {
+  unowned let simulator: FBSimulator
+  let writer: Writer
+
+  init(simulator: FBSimulator, writer: Writer) {
+    self.simulator = simulator
+    self.writer = writer
+    super.init()
+    self.simulator.userEventSink = self
+  }
+
+  public func containerApplicationDidLaunch(applicationProcess: FBProcessInfo!) {
+    self.simulatorEvent()
+  }
+
+  public func containerApplicationDidTerminate(applicationProcess: FBProcessInfo!, expected: Bool) {
+    self.simulatorEvent()
+  }
+
+  public func simulatorDidLaunch(launchdSimProcess: FBProcessInfo!) {
+    self.simulatorEvent()
+  }
+
+  public func simulatorDidTerminate(launchdSimProcess: FBProcessInfo!, expected: Bool) {
+    self.simulatorEvent()
+  }
+
+  public func agentDidLaunch(launchConfig: FBAgentLaunchConfiguration!, didStart agentProcess: FBProcessInfo!, stdOut: NSFileHandle!, stdErr: NSFileHandle!) {
+    self.simulatorEvent()
+  }
+
+  public func agentDidTerminate(agentProcess: FBProcessInfo!, expected: Bool) {
+    self.simulatorEvent()
+  }
+
+  public func applicationDidLaunch(launchConfig: FBApplicationLaunchConfiguration!, didStart applicationProcess: FBProcessInfo!, stdOut: NSFileHandle!, stdErr: NSFileHandle!) {
+    self.simulatorEvent()
+  }
+
+  public func applicationDidTerminate(applicationProcess: FBProcessInfo!, expected: Bool) {
+    self.simulatorEvent()
+  }
+
+  public func diagnosticInformationAvailable(name: String!, process: FBProcessInfo!, value: protocol<NSCoding, NSCopying>!) {
+    self.simulatorEvent()
+  }
+
+  public func didChangeState(state: FBSimulatorState) {
+    self.simulatorEvent()
+  }
+
+  public func terminationHandleAvailable(terminationHandle: FBTerminationHandleProtocol!) {
+    self.simulatorEvent()
+  }
+
+  public func report(eventName: EventName, _ eventType: EventType, _ subject: EventReporterSubject) {
+    do {
+      self.writer.write(
+        try JSON.serializeToString([
+          "simulator" : self.simulator.jsonSerializableRepresentation(),
+          "event_name" : eventName.rawValue,
+          "event_type" : eventType.rawValue,
+          "subject" : subject.jsonSerializableRepresentation()
+          ])
+      )
+    } catch {
+
+    }
+  }
+
+  public func simulatorEvent() {
+    do {
+      self.writer.write(try JSON.serializeToString(self.simulator))
+    } catch {
+      
+    }
+  }
+}
