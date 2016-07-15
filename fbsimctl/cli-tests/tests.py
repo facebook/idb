3eb58add052a493bb4199b9b1b61ec8b56c5b3eb
Lawrence Lomax <lawrencelomax@fb.com>
2016-07-15 05:18:55 -0700

    Add CLI Tests for fbsimctl
    Summary:
    There are a number of Unit & Integration tests for `fbsimctl` via the `FBSimulatorControlKit` target. These tests however, don't test the full lifecycle of the process from start to finish. `FBSimulatorControlKitTests` all execute within one process lifecycle and do not test the main entry-point of the Application.
    
    This diff adds the basics of a Python3 based e2e-testing suite for testing the `fbsimctl` executable through multiple invocations of the `fbsimctl` executable. It uses the `--json` argument and wraps these invocations in python objects and conveniences in order to make driving the tests easier. There is a base `unittest` class that adds a number of assertions that use these utilities. It should be easy to take this base and extend it to test many of the actions and automation scenarios for `fbsimctl`. I may also look at adding some nice logging to make it possible to understand how `fbsimctl` is being driven, which may help developers who wish to integrate `fbsimctl` into their own projects.
    
    I will follow this up with extensions to the build script and travis manifest, so that we can run this on travis ci. The test script currently assumes that `fbsimctl` is on the path, but we will be able to change this to point at a build that we create in CI and pass this through, so we can test regressions on the cli iteself
    
    Reviewed By: migchez
    
    Differential Revision: D3569094
    
    fbshipit-source-id: d7680e124dbfe1595498095110f5b7c2ef7c3799

---
+++ fbsimctl/cli-tests/tests.py
@@ -0,0 +1,94 @@
+from util import (FBSimctl, Simulator)
+import unittest
+import tempfile
+
+EXECUTABLE_PATH = 'fbsimctl'
+
+
+class FBSimctlTestCase(unittest.TestCase):
+    def setUp(self):
+        self.fbsimctl = FBSimctl(EXECUTABLE_PATH, self.provideSetPath())
+
+    def provideSetPath(self):
+        return None
+
+    def assertEventSuccesful(self, arguments, event_name):
+        events = self.fbsimctl.run(arguments)
+        matching_events = events.matching(event_name, 'ended')
+        match_count = len(matching_events)
+        self.assertNotEqual(
+            0,
+            match_count,
+            'Expected one successful {} event, but there were none. Other events: \b {}'.format(
+                event_name,
+                events
+            )
+        )
+        self.assertLess(
+            match_count,
+            2,
+            'Expected one successful {} event, but there were {}. Matching events: \b {}'.format(
+                event_name,
+                str(match_count),
+                matching_events,
+            )
+        )
+        return matching_events[0]
+
+    def assertListContainsOnly(self, expected_udids):
+        events = self.fbsimctl.run(['list'])
+        list_events = events.matching('list', 'discrete')
+        list_udids = [
+            event.get('subject').get('udid') for event in list_events
+        ]
+        remainder = set(expected_udids) - set(list_udids)
+        self.assertEqual(
+            len(remainder),
+            0,
+            'Expected list command to contain {}, but it was missing'.format(
+                remainder
+            )
+        )
+        additional = set(list_udids) - set(expected_udids)
+        self.assertEqual(
+            len(additional),
+            0,
+            'Expected list command only contain {}, but it had additional {}'.format(
+                expected_udids,
+                additional,
+            )
+        )
+        return list_events
+
+    def assertExtractSimulator(self, json_event):
+        sim_json = json_event.get('subject')
+        self.assertIsNotNone(
+            sim_json,
+            'Expected {} to contain a simulator, but it did not'.format(
+                json_event,
+            )
+        )
+        return Simulator(sim_json)
+
+
+class TestDefaultDeviceSet(FBSimctlTestCase):
+    def testList(self):
+        self.fbsimctl(['list'])
+
+    def testCommandThatDoesNotExist(self):
+        with self.assertRaises(Exception):
+            self.fbsimctl(['foo'])
+
+
+class TestCustomDeviceSet(FBSimctlTestCase):
+    def provideSetPath(self):
+        return tempfile.mkdtemp()
+
+    def testCreateDeleteiPhone6(self):
+        self.assertListContainsOnly([])
+        simulator = self.assertExtractSimulator(
+            self.assertEventSuccesful(['create', 'iPhone 6'], 'create')
+        )
+        self.assertListContainsOnly([simulator.get_udid()])
+        self.assertEventSuccesful([simulator.get_udid(), 'delete'], 'delete')
+        self.assertListContainsOnly([])
---
+++ fbsimctl/cli-tests/util.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import io
+import json
+import subprocess
+import logging
+
+DEFAULT_TIMEOUT = 100
+
+# Setup the Logger
+logging.basicConfig(format='%(message)s')
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+
+class Events:
+    def __init__(self, events):
+        self.__events = events
+
+    def __repr__(self):
+        return '\n'.join(
+            [str(event) for event in self.__events],
+        )
+
+    def matching(self, event_name, event_type):
+        return [
+            event for event in self.__events
+            if event['event_name'] == event_name and event['event_type'] == event_type
+        ]
+
+
+class Simulator:
+    def __init__(self, json):
+        self.__json = json
+
+    def __repr__(self):
+        return str(self.__json)
+
+    def get_udid(self):
+        return self.__json['udid']
+
+
+class FBSimctl:
+    def __init__(self, executable_path, set_path=None):
+        self.__executable_path = executable_path
+        self.__set_path = set_path
+
+    def __call__(self, arguments):
+        return self.run(arguments)
+
+    def _make_arguments(self, arguments=[]):
+        base_arguments = [self.__executable_path]
+        if self.__set_path:
+            base_arguments += ['--set', self.__set_path]
+        base_arguments.append('--json')
+        return base_arguments + arguments
+
+    def _make_process(self, arguments=[]):
+        return subprocess.Popen(
+            arguments,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=False,
+        )
+
+    def run(self, arguments, timeout=DEFAULT_TIMEOUT):
+        arguments = self._make_arguments(arguments)
+        with self._make_process(arguments) as process:
+            process.wait(timeout=DEFAULT_TIMEOUT)
+            events = []
+            if process.returncode != 0:
+                raise Exception(
+                    '{0} resulted in a non-zero return code {1} {2}'.format(
+                        arguments,
+                        process.returncode,
+                        events,
+                    )
+                )
+            with io.TextIOWrapper(process.stdout, encoding='utf-8') as text:
+                for line in text:
+                    events += [json.loads(line)]
+            return Events(
+                events
+            )
