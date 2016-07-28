78f28e5bc0d31d2fa756c67ececa91ea1a119059
Johannes Plunien <plu@pqpq.de>
2016-07-28 06:57:47 +0400

    Set nullability contract

--- XCTestBootstrap/TestManager/FBTestManagerTestReporterComposite.h
+++ XCTestBootstrap/TestManager/FBTestManagerTestReporterComposite.h
@@ -10,6 +10,8 @@
 #import "FBTestManagerTestReporter.h"
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FBTestManagerTestReporterComposite : NSObject <FBTestManagerTestReporter>
 
 /**
@@ -18,3 +20,5 @@
 + (instancetype)withTestReporters:(NSArray<id<FBTestManagerTestReporter>> *)reporters;
 
 @end
+
+NS_ASSUME_NONNULL_END
