2d43765df6b54b3a36f961496ac8464a6da9ea4a
Johannes Plunien <plu@pqpq.de>
2016-08-14 14:01:02 +0400

    Add some fixtures to FBXCTestKit

---
+++ fbxctest/FBXCTestKitTests/Fixtures/FBXCTestKitFixtures.h
@@ -0,0 +1,32 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#import <Foundation/Foundation.h>
+
+@class FBApplicationDescriptor;
+
+@interface FBXCTestKitFixtures : NSObject
+
+/**
+ Creates a new temporary directory.
+
+ @return path to temporary directory.
+ */
++ (NSString *)createTemporaryDirectory;
+
+/**
+ A build of Apple's 'Table Search' Sample Application.
+ Source is available at:
+ https://developer.apple.com/library/ios/samplecode/TableSearch_UISearchController/Introduction/Intro.html#//apple_ref/doc/uid/TP40014683
+
+ @return path to the application.
+ */
++ (NSString *)applicationPath;
+
+/**
+ An Application Test xctest bundle.
+
+ @return path to the Application Test Bundle.
+ */
++ (NSString *)testBundlePath;
+
+@end
---
+++ fbxctest/FBXCTestKitTests/Fixtures/FBXCTestKitFixtures.m
@@ -0,0 +1,33 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#import "FBXCTestKitFixtures.h"
+
+#import <FBSimulatorControl/FBSimulatorControl.h>
+
+@implementation FBXCTestKitFixtures
+
++ (NSString *)createTemporaryDirectory
+{
+  NSError *error;
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+
+  NSString *temporaryDirectory =
+      [NSTemporaryDirectory() stringByAppendingPathComponent:[[NSProcessInfo processInfo] globallyUniqueString]];
+  [fileManager createDirectoryAtPath:temporaryDirectory withIntermediateDirectories:YES attributes:nil error:&error];
+  NSAssert(!error, @"Could not create temporary directory");
+
+  return temporaryDirectory;
+}
+
++ (NSString *)applicationPath
+{
+  return [[[NSBundle bundleForClass:self] pathForResource:@"TableSearch" ofType:@"app"]
+      stringByAppendingPathComponent:@"TableSearch"];
+}
+
++ (NSString *)testBundlePath
+{
+  return [[NSBundle bundleForClass:self] pathForResource:@"SimpleTestTarget" ofType:@"xctest"];
+}
+
+@end
