f3c5e75ab133e265cda1314b087d8e9bb2c7007b
Johannes Plunien <plu@pqpq.de>
2016-08-15 03:39:49 -0700

    Extract FBXCTestKit.framework
    Summary:
    * Extract FBXCTestKit.framework
    * Add simple integration test
    * Support building and testing `fbxctest` via build script
    * Build and test `fbxctest` on Travis
    Closes https://github.com/facebook/FBSimulatorControl/pull/299
    
    Reviewed By: k21
    
    Differential Revision: D3716034
    
    Pulled By: lawrencelomax
    
    fbshipit-source-id: 60a11784d225264101be60d19b92d6a7ea631a18

---
+++ fbxctest/FBXCTestKit/FBXCTestKit.h
@@ -0,0 +1,6 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#import <FBXCTestKit/FBJSONTestReporter.h>
+#import <FBXCTestKit/FBTestRunConfiguration.h>
+#import <FBXCTestKit/FBXCTestBootstrapper.h>
+#import <FBXCTestKit/FBXCTestRunner.h>
