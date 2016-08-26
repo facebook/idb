9f57d332db71055aa5e34cf06c351a0bccfc238b
Jia Li <jial@fb.com>
2016-08-25 19:16:16 -0700

    fix a test crash
    Summary: a recent diff D3764512 calls processIDWithBundleID w/o noticing this api does an assert on parameter error.
    
    Reviewed By: sigmix
    
    Differential Revision: D3774187
    
    fbshipit-source-id: 65a5dfb8cd0ab084a091e5548ad89ccbd9499b6a

--- XCTestBootstrap/TestManager/FBTestBundleConnection.m
+++ XCTestBootstrap/TestManager/FBTestBundleConnection.m
@@ -336,7 +336,8 @@ typedef NS_ENUM(NSUInteger, FBTestBundleConnectionState) {
   // Set the Crash Date Check.
   self.lastCrashCheckDate = NSDate.date;
 
-  pid_t pid = [self.deviceOperator processIDWithBundleID:self.context.testRunnerBundleID error:nil];
+  NSError *error;
+  pid_t pid = [self.deviceOperator processIDWithBundleID:self.context.testRunnerBundleID error:&error];
   if (pid >= 1) {
     return nil;
   }
