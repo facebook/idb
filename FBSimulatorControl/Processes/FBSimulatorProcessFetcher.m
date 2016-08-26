516b6c02018700a78980b15b8fe83a8ab8efae12
Lawrence Lomax <lawrencelomax@fb.com>
2016-08-26 06:26:54 -0700

    Use original Simulator.app Process Fetching
    Summary: I'm not sure why this is broken on Travis, but I'll have to take a look later. Reverting this back to an earlier implementation that is tried-and-true
    
    Reviewed By: marekcirkos
    
    Differential Revision: D3777361
    
    fbshipit-source-id: 0f43c47a37c9873892ec21603f543ae7a9315fef

--- FBSimulatorControl/Processes/FBSimulatorProcessFetcher.m
+++ FBSimulatorControl/Processes/FBSimulatorProcessFetcher.m
@@ -42,8 +42,7 @@ NSString *const FBSimulatorControlSimulatorLaunchEnvironmentSimulatorUDID = @"FB
   // All Simulator Versions from Xcode 5-7, have Simulator.app in their path:
   // iOS Simulator.app/Contents/MacOS/iOS Simulator
   // Simulator.app/Contents/MacOS/Simulator
-  NSArray<NSDictionary<NSString *, id> *> *jobs = [FBServiceManagement jobsWithProgramWithLaunchPathSubstring:@"Simulator.app"];
-  return [self.processFetcher processInfoForJobDictionaries:jobs];
+  return [self.processFetcher processesWithLaunchPathSubstring:@"Simulator.app"];
 }
 
 - (NSDictionary<NSString *, FBProcessInfo *> *)simulatorApplicationProcessesByUDIDs:(NSArray<NSString *> *)udids unclaimed:(NSArray<FBProcessInfo *> *_Nullable * _Nullable)unclaimedOut
