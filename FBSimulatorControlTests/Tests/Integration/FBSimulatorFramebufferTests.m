b7623c1ea8cc41f6a333814aaa5f2f59cb7f1eba
Lawrence Lomax <lawrencelomax@fb.com>
2016-09-15 07:49:00 -0700

    Add Test for recording video of running Simulator App
    Summary: In Xcode 8, it now becomes possible because of the `IOSurface` to Launch with the `Simulator.app` and then record it. We can write a test for this!
    
    Reviewed By: marekcirkos
    
    Differential Revision: D3862811
    
    fbshipit-source-id: 2f15e9bcd4cd9c143ea4cc201b39e88882f3e81d

---
+++ FBSimulatorControlTests/Tests/Integration/FBSimulatorFramebufferTests.m
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import <FBSimulatorControl/FBSimulatorControl.h>
+
+#import "FBSimulatorControlAssertions.h"
+#import "FBSimulatorControlFixtures.h"
+#import "FBSimulatorControlTestCase.h"
+
+@interface FBSimulatorFramebufferTests : FBSimulatorControlTestCase
+
+@end
+
+@implementation FBSimulatorFramebufferTests
+
+- (void)testRecordsVideoForSimulatorApp
+{
+  FBSimulatorLaunchConfiguration *launchConfiguration = self.simulatorLaunchConfiguration;
+  if (launchConfiguration.shouldUseDirectLaunch) {
+    NSLog(@"Skipping running -[%@ %@] since the Simulator will be launched directly", NSStringFromClass(self.class), NSStringFromSelector(_cmd));
+    return;
+  }
+  if (!FBControlCoreGlobalConfiguration.isXcode8OrGreater) {
+    NSLog(@"Skipping running -[%@ %@] since Xcode 8 or greater is required", NSStringFromClass(self.class), NSStringFromSelector(_cmd));
+    return;
+  }
+
+  FBSimulator *simulator = [self assertObtainsBootedSimulatorWithConfiguration:self.simulatorConfiguration launchConfiguration:launchConfiguration];
+  [self assertSimulator:simulator launchesApplication:self.safariApplication withApplicationLaunchConfiguration:self.safariAppLaunch];
+  [self assertInteractionSuccessful:[simulator.interact startRecordingVideo]];
+  [self assertSimulator:simulator launchesApplication:self.tableSearchApplication withApplicationLaunchConfiguration:self.tableSearchAppLaunch];
+}
+
+@end
