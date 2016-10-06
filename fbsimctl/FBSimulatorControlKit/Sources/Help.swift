9d32607e5c2acb99ae9b8d80fee6de818b8eb7cb
Ashok Menon <ashokmenon@fb.com>
2016-10-06 15:17:48 -0700

    Adding framework to capture CLI argument parser formats
    Summary:
    This diff introduces a framework that can be used to annotate the `fbsimctl` CLI argument parser with metadata (`ParserDescription`s) that can then be used to generate a help/usage dialog.
    
    I have modified the current help dialog to show an example of how the metadata is formatted into a Help dialog (I will update the example with something more substantial shortly), before this diff is landed, this will be removed.
    
    The metadata is represented as a tree, roughly corresponding to a regular grammar in which certain nodes are annotated with usage information. Special section nodes can be used to break up longer formats into logically related pieces. When formatting the help dialog, we only print information about each section and each primitive once, and we only print information about each flag once per section.
    
    The purpose of this diff is to nail down details about how the dialog should be formatted. For example, currently I use square brackets to wrap around section tags that appear in other descriptions and to wrap around formats that need to be delimited (For example `[--foo | --bar]`) as they appear nested within other formats. Perhaps we could use a different syntax for one or the other?
    
    Differential Revision: D3950909
    
    fbshipit-source-id: 93f8e8316a1fa76f2eba4ccd6a87baa1fed24502

--- fbsimctl/FBSimulatorControlKit/Sources/Help.swift
+++ fbsimctl/FBSimulatorControlKit/Sources/Help.swift
@@ -10,7 +10,61 @@
 import Foundation
 
 extension Help : CustomStringConvertible {
-  public var description: String { get {
-    return "Help"
-  }}
+  public var description: String {
+
+    let classPrim = PrimitiveDesc(name: "class", desc: "A class name.")
+    let filePrim = PrimitiveDesc(name: "file", desc: "A file URI.")
+    let dirPrim = PrimitiveDesc(name: "dir", desc: "A directory URI.")
+
+    func fileOpt(_ action: String) -> ParserDescription {
+      return
+        SequenceDesc(children: [
+          FlagDesc(name: "file", desc: "The file to \(action)"),
+          filePrim
+        ])
+    }
+
+    func dirOpt(_ action: String) -> ParserDescription {
+      return
+        SequenceDesc(children: [
+          FlagDesc(name: "dir", desc: "The directory to \(action)"),
+          dirPrim
+        ])
+    }
+
+    let buildSection = SectionDesc(tag: "build-opts", name: "Building",
+                                   desc: "Build specific targets.",
+                                   child: ChoiceDesc(children: [
+                                     classPrim,
+                                     fileOpt("build"),
+                                     dirOpt("build")
+                                   ]))
+
+    let testSection = SectionDesc(tag: "test-opts", name: "Testing",
+                                  desc: "Run tests on the given targets.",
+                                  child: ChoiceDesc(children: [
+                                    classPrim,
+                                    fileOpt("test"),
+                                    dirOpt("test")
+                                  ]))
+
+    let choices = ChoiceDesc(children: [
+      FlagDesc(name: "version", desc: "Print the version"),
+      FlagDesc(name: "help", desc: "Print the help dialog"),
+      buildSection,
+      testSection
+    ])
+
+    let app = SequenceDesc(children: [
+      CmdDesc(cmd: "fbsimctl"),
+      choices
+    ])
+
+    let desc = SectionDesc(tag: "fbsimctl", name: "Help",
+                           desc: "This is an example help/usage dialog built using `ParserDescription`. This is not the actual dialog for `fbsimctl`",
+                           child: app)
+
+    // return CLI.parser.description
+    return desc.normalised.usage
+  }
 }
---
+++ fbsimctl/FBSimulatorControlKitTests/Tests/Unit/ParserDescriptionTests.swift
@@ -0,0 +1,319 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import XCTest
+import FBSimulatorControl
+@testable import FBSimulatorControlKit
+
+/**
+ * FakeDesc
+ *
+ * Fake, probe-able ParserDescription
+ */
+struct FakeDesc : ParserDescription {
+  public let summary: String
+  public let isDelimited: Bool
+  public let children: [ParserDescription]
+  public let isNormalised: Bool
+
+  init(summary: String,
+       isDelimited: Bool,
+       children: [ParserDescription],
+       isNormalised: Bool) {
+    self.summary = summary
+    self.isDelimited = isDelimited
+    self.children = children
+    self.isNormalised = isNormalised
+  }
+
+  init(_ n : Int, WithChildren children: [ParserDescription]) {
+    self.init(summary: "fake-desc-"  + String(n),
+              isDelimited: false,
+              children: children,
+              isNormalised: false)
+  }
+
+  init(_ n : Int){
+    self.init(n, WithChildren: [])
+  }
+
+  public var normalised: ParserDescription {
+    return FakeDesc(summary: summary,
+                    isDelimited: isDelimited,
+                    children: children.map {$0.normalised},
+                    isNormalised: true)
+  }
+}
+
+/**
+ * AssertCast<U, T>(_ obj: U, _ tests: (T) -> Void)
+ *
+ * Check whether `obj : U` can be dynamically cast to a value of type `T`, and
+ * if it can, pass it on to a continuation for potential further testing.
+ */
+func AssertCast<U, T>(_ obj: U, _ tests: (T) -> Void) {
+  switch obj {
+  case let casted as T:
+    tests(casted)
+    break;
+  default:
+    XCTFail("AssertCast: Could not dynamically cast value")
+    break
+  }
+}
+
+/**
+ * AssertEqualDesc(_ lhs: ParserDescription, _ rhs: ParserDescription)
+ *
+ * Check whether the descriptions `lhs` and `rhs` are observationally
+ * equivalent.
+ */
+func AssertEqualDesc(_ lhs: ParserDescription, _ rhs: ParserDescription) {
+
+  XCTAssertEqual(lhs.summary, rhs.summary)
+  XCTAssertEqual(lhs.delimitedSummary, rhs.delimitedSummary)
+  XCTAssertEqual(lhs.isDelimited, rhs.isDelimited)
+  XCTAssertEqual(lhs.description, rhs.description)
+
+  let lcs = lhs.children
+  let rcs = rhs.children
+
+  XCTAssertEqual(lcs.count, rcs.count)
+  for (l, r) in zip(lcs, rcs) {
+    AssertEqualDesc(l, r);
+  }
+}
+
+class NormalisationTests : XCTestCase {
+
+  func testPrimitive() {
+    let prim = PrimitiveDesc(name: "name", desc: "desc")
+    AssertEqualDesc(prim, prim.normalised)
+  }
+
+  func testFlag() {
+    let flag = FlagDesc(name: "name", desc: "desc")
+    AssertEqualDesc(flag, flag.normalised)
+  }
+
+  func testCmd() {
+    let cmd = CmdDesc(cmd: "cmd")
+    AssertEqualDesc(cmd, cmd.normalised)
+  }
+
+  func testSection() {
+    let sect = SectionDesc(tag: "tag", name: "name", desc: "desc",
+                           child: FakeDesc(1))
+    let norm = sect.normalised
+    AssertEqualDesc(sect, norm)
+    AssertCast(norm) { (norm: SectionDesc) in
+      AssertCast(norm.child) { (child: FakeDesc) in
+        XCTAssertTrue(child.isNormalised)
+      }
+    }
+  }
+
+  func testAtleast() {
+    let atleast = AtleastDesc(lowerBound: 1,
+                              child: FakeDesc(1))
+    let norm = atleast.normalised
+    AssertEqualDesc(atleast, norm)
+    AssertCast(norm) { (norm: AtleastDesc) in
+      AssertCast(norm.child) { (child: FakeDesc) in
+        XCTAssertTrue(child.isNormalised)
+      }
+    }
+  }
+
+  func testOptional() {
+    let opt = OptionalDesc(child: FakeDesc(1))
+    AssertEqualDesc(opt, opt.normalised)
+  }
+
+  func testOptionalNonEmptyFlattening() {
+    let nonEmpty = AtleastDesc(lowerBound: 1,
+                               child: FakeDesc(1),
+                               sep: FakeDesc(2))
+    let opt = OptionalDesc(child: nonEmpty)
+
+    let expected = AtleastDesc(lowerBound: 0,
+                               child: nonEmpty.child,
+                               sep: nonEmpty.sep)
+
+    AssertEqualDesc(opt.normalised, expected)
+  }
+
+  func testOptionalAtleastPreserve() {
+    let atleast = AtleastDesc(lowerBound: 2,
+                              child: FakeDesc(1),
+                              sep: FakeDesc(2))
+    let opt = OptionalDesc(child: atleast)
+
+    AssertEqualDesc(opt, opt.normalised)
+  }
+
+  func testSequenceDiscarding() {
+    let fake = FakeDesc(1)
+    let seq = SequenceDesc(children: [fake])
+    let norm = seq.normalised
+
+    AssertEqualDesc(fake, norm)
+    AssertCast(norm) { (actual: FakeDesc) in
+      XCTAssertTrue(actual.isNormalised)
+    }
+  }
+
+  func testSequenceFlattening() {
+    let seq = SequenceDesc(children: [
+      FakeDesc(1),
+      FakeDesc(2),
+      SequenceDesc(children: [
+        FakeDesc(3),
+        SequenceDesc(children: [
+          FakeDesc(4)
+          ])
+        ]),
+      FakeDesc(5, WithChildren: [
+        SequenceDesc(children: [
+          FakeDesc(6)
+          ]),
+        FakeDesc(7)
+        ])
+      ])
+
+    let expected = SequenceDesc(children: [
+      FakeDesc(1),
+      FakeDesc(2),
+      FakeDesc(3),
+      FakeDesc(4),
+      FakeDesc(5, WithChildren: [
+        FakeDesc(6),
+        FakeDesc(7)
+        ])
+      ])
+
+    AssertEqualDesc(expected, seq.normalised)
+  }
+
+  func testSequenceEmpty() {
+    let seq = SequenceDesc(children: [])
+    AssertEqualDesc(seq, seq.normalised)
+  }
+
+  func testChoiceDiscarding() {
+    let fake = FakeDesc(1)
+    let choices = ChoiceDesc(children: [fake])
+    let norm = choices.normalised
+
+    AssertEqualDesc(fake, norm)
+    AssertCast(norm) { (actual: FakeDesc) in
+      XCTAssertTrue(actual.isNormalised)
+    }
+  }
+
+  func testChoiceFlattening() {
+    let choices = ChoiceDesc(children: [
+      FakeDesc(1),
+      FakeDesc(2),
+      ChoiceDesc(children: [
+        FakeDesc(3),
+        ChoiceDesc(children: [
+          FakeDesc(4)
+          ])
+        ]),
+      FakeDesc(5, WithChildren: [
+        ChoiceDesc(children: [
+          FakeDesc(6)
+          ]),
+        FakeDesc(7)
+        ])
+      ])
+
+    let expected = ChoiceDesc(children: [
+      FakeDesc(1),
+      FakeDesc(2),
+      FakeDesc(3),
+      FakeDesc(4),
+      FakeDesc(5, WithChildren: [
+        FakeDesc(6),
+        FakeDesc(7)
+        ])
+      ])
+
+    AssertEqualDesc(expected, choices.normalised)
+  }
+
+  func testChoiceEmpty() {
+    let choice = ChoiceDesc(children: [])
+    AssertEqualDesc(choice, choice.normalised)
+  }
+
+  func testPreservesExpandednessOfChoice() {
+    let choice = ChoiceDesc(children: [FakeDesc(1), FakeDesc(2)]).expanded
+    AssertEqualDesc(choice, choice.normalised)
+  }
+}
+
+class DelimitedSummaryTest : XCTestCase {
+  func testNotDelimited() {
+    let fake = FakeDesc(summary: "summary",
+                        isDelimited: false,
+                        children: [],
+                        isNormalised: false)
+
+    XCTAssertEqual("{{ summary }}", fake.delimitedSummary)
+  }
+
+  func testDelimited() {
+    let fake = FakeDesc(summary: "summary",
+                        isDelimited: true,
+                        children: [],
+                        isNormalised: false)
+    XCTAssertEqual("summary", fake.delimitedSummary)
+  }
+}
+
+class FindAllTests : XCTestCase {
+  func testSingleton() {
+    let fake = FakeDesc(1)
+    var fakes = [FakeDesc]()
+    fake.findAll(&fakes)
+
+    XCTAssertEqual(0, fakes.count)
+  }
+
+  func testNested() {
+    let fake4 = FakeDesc(4)
+    let fake3 = FakeDesc(3, WithChildren: [fake4])
+    let fake2 = FakeDesc(2)
+    let fake1 = FakeDesc(1, WithChildren: [fake2, fake3])
+
+    let expectedFakes = [fake2, fake3, fake4]
+    var actualFakes = [FakeDesc]()
+    fake1.findAll(&actualFakes)
+
+    XCTAssertEqual(expectedFakes.count, actualFakes.count)
+    for fake in expectedFakes {
+      XCTAssert(actualFakes.contains { $0.summary == fake.summary })
+    }
+  }
+
+  func testHiddenBySect() {
+    let fake2 = FakeDesc(2)
+    let fake1 = FakeDesc(1, WithChildren: [
+      fake2,
+      SectionDesc(tag: "tag", name: "name", desc: "desc", child: FakeDesc(3))
+    ])
+
+    var actualFakes = [FakeDesc]()
+    fake1.findAll(&actualFakes)
+    XCTAssertEqual(1, actualFakes.count)
+    AssertEqualDesc(fake2, actualFakes.first!)
+  }
+}
