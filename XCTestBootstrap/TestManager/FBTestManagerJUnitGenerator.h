2e94528342e4d548cbaa2ef9fb9d94227c1bd7d8
Johannes Plunien <plu@pqpq.de>
2017-01-29 09:59:23 +0400

    Extract FBTestManagerJUnitGenerator
    Extracting the JUnit XML document generator into a separate method makes
    it possible to merge together several test suites into one single graph
    of test suites. It also adds the option to sort them into groups using a
    package prefix. Unfortunately the JUnit parser on Jenkins does not
    directly use the package attribute, instead the classname attribute must
    have a Java-style package prefix (e.g. com.facebook.FBTestCase).

---
+++ XCTestBootstrap/TestManager/FBTestManagerJUnitGenerator.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class FBTestManagerTestReporterTestSuite;
+
+/**
+ Transforms a graph of FBTestManagerTestReporterTestSuite objects into
+ an NSXMLDocument representation of the JUnit format.
+ */
+@interface FBTestManagerJUnitGenerator : NSObject
+
+/**
+ Generates JUnit XML document for given test suite.
+
+ @param testSuite the test suite to transform.
+ @return an NSXMLDocument instance.
+ */
++ (NSXMLDocument *)documentForTestSuite:(FBTestManagerTestReporterTestSuite *)testSuite;
+
+/**
+ Generates JUnit XML document for given array of test suite elements.
+
+ @param testSuiteElements the test suite XML element.
+ @return an NSXMLDocument instance.
+ */
++ (NSXMLDocument *)documentForTestSuiteElements:(NSArray<NSXMLElement *> *)testSuiteElements;
+
+/**
+ Generates an XML node for the given Test Suite object and prefixes all Test Case names
+ with the given package prefix.
+
+ @param testSuite the test suite to transform.
+ @param packagePrefix the package prefix to prepend on each test case class name.
+ @return an NSXMLDocument instance.
+ */
++ (NSXMLElement *)elementForTestSuite:(FBTestManagerTestReporterTestSuite *)testSuite packagePrefix:(nullable NSString *)packagePrefix;
+
+@end
+
+NS_ASSUME_NONNULL_END
