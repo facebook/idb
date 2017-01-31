f08ca3c6c808a9caeeacee330f1d018da646396b
Johannes Plunien <plu@pqpq.de>
2017-01-31 15:46:52 +0400

    Skip e2e-test if Metal is not supported

---
+++ fbsimctl/cli-tests/supports_metal.swift
@@ -0,0 +1,10 @@
+import Metal
+
+// Exits non-zero if Metal is supported by the OS version but not
+// available on the current hardware. Is used in fbsimctl's
+// e2e-tests to see if video recording can be tested or not.
+if #available(OSX 10.11, *) {
+  if MTLCreateSystemDefaultDevice() == nil {
+    exit(1)
+  }
+}
