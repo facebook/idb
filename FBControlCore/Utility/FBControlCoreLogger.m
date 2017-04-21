9de29d16e30965ef34afc7dd5073fa2602183a54
Lawrence Lomax <lawrencelomax@fb.com>
2017-04-21 05:43:01 -0700

    Support Logging to file on macOS 10.12 (Again)
    Summary: The earlier diff that introduced this broke for logger methods that took a second parameter. This ensures that `-[NSObject performSelector:object:]` is called so that the derived loggers are indeed correct.
    
    Reviewed By: marekcirkos
    
    Differential Revision: D4905768
    
    fbshipit-source-id: 6e29ba4f9915e2ceccb70ecf1dea90204953437f

--- FBControlCore/Utility/FBControlCoreLogger.m
+++ FBControlCore/Utility/FBControlCoreLogger.m
@@ -11,6 +11,157 @@
 
 #import <asl.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+
+@interface FBControlCoreLogger_Composite : NSObject <FBControlCoreLogger>
+
+@property (nonatomic, strong, readonly) NSArray<id<FBControlCoreLogger>> *loggers;
+
+@end
+
+@implementation FBControlCoreLogger_Composite
+
+- (instancetype)initWithLoggers:(NSArray<id<FBControlCoreLogger>> *)loggers
+{
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
+
+  _loggers = loggers;
+  return self;
+}
+
+- (id<FBControlCoreLogger>)log:(NSString *)string
+{
+  for (id<FBControlCoreLogger> logger in self.loggers) {
+    [logger log:string];
+  }
+  return self;
+}
+
+- (id<FBControlCoreLogger>)logFormat:(NSString *)format, ...
+{
+  va_list args;
+  va_start(args, format);
+  NSString *string = [[NSString alloc] initWithFormat:format arguments:args];
+  va_end(args);
+
+  return [self log:string];
+}
+
+- (id<FBControlCoreLogger>)info
+{
+  return [self loggerByApplyingSelector:_cmd];
+}
+
+- (id<FBControlCoreLogger>)debug
+{
+  return [self loggerByApplyingSelector:_cmd];
+}
+
+- (id<FBControlCoreLogger>)error
+{
+  return [self loggerByApplyingSelector:_cmd];
+}
+
+- (id<FBControlCoreLogger>)onQueue:(dispatch_queue_t)queue
+{
+  return [self loggerByApplyingSelector:_cmd object:queue];
+}
+
+- (id<FBControlCoreLogger>)withPrefix:(NSString *)prefix
+{
+  return [self loggerByApplyingSelector:_cmd object:prefix];
+}
+
+- (id<FBControlCoreLogger>)loggerByApplyingSelector:(SEL)selector
+{
+  NSMutableArray<id<FBControlCoreLogger>> *loggers = [NSMutableArray arrayWithCapacity:self.loggers.count];
+  for (id<FBControlCoreLogger> logger in self.loggers) {
+    [loggers addObject:[logger performSelector:selector]];
+  }
+  return [[FBControlCoreLogger_Composite alloc] initWithLoggers:[loggers copy]];
+}
+
+- (id<FBControlCoreLogger>)loggerByApplyingSelector:(SEL)selector object:(id)object
+{
+  NSMutableArray<id<FBControlCoreLogger>> *loggers = [NSMutableArray arrayWithCapacity:self.loggers.count];
+  for (id<FBControlCoreLogger> logger in self.loggers) {
+    [loggers addObject:[logger performSelector:selector withObject:object]];
+  }
+  return [[FBControlCoreLogger_Composite alloc] initWithLoggers:[loggers copy]];
+}
+
+@end
+
+@interface FBControlCoreLogger_File : NSObject <FBControlCoreLogger>
+
+@property (nonatomic, assign, readonly) int descriptor;
+
+@end
+
+@implementation FBControlCoreLogger_File
+
+const char *NewLine = "\n";
+
+- (instancetype)initWithFileDescriptor:(int)descriptor
+{
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
+
+  _descriptor = descriptor;
+  return self;
+}
+
+- (id<FBControlCoreLogger>)log:(NSString *)string
+{
+  const char *data = string.UTF8String;
+  write(self.descriptor, data, strlen(data));
+  write(self.descriptor, NewLine, strlen(NewLine));
+  return self;
+}
+
+- (id<FBControlCoreLogger>)logFormat:(NSString *)format, ...
+{
+  va_list args;
+  va_start(args, format);
+  NSString *string = [[NSString alloc] initWithFormat:format arguments:args];
+  va_end(args);
+
+  return [self log:string];
+}
+
+- (id<FBControlCoreLogger>)info
+{
+  return self;
+}
+
+- (id<FBControlCoreLogger>)debug
+{
+  return self;
+}
+
+- (id<FBControlCoreLogger>)error
+{
+  return self;
+}
+
+- (id<FBControlCoreLogger>)onQueue:(dispatch_queue_t)queue
+{
+  return self;
+}
+
+- (id<FBControlCoreLogger>)withPrefix:(NSString *)prefix
+{
+  return self;
+}
+
+@end
+
 @interface FBASLClientWrapper : NSObject
 
 @property (nonatomic, assign, readonly) asl_object_t client;
@@ -172,6 +323,19 @@
 
 + (id<FBControlCoreLogger>)systemLoggerWritingToFileDescriptor:(int)fileDescriptor withDebugLogging:(BOOL)debugLogging
 {
+  // asl_add_output_file does not work in macOS 10.2, so we need a composite logger to write to a file descriptor.
+  if (NSProcessInfo.processInfo.operatingSystemVersion.minorVersion < 12) {
+    return [self aslLoggerWritingToFileDescriptor:fileDescriptor withDebugLogging:debugLogging];
+  }
+  FBControlCoreLogger_ASL *aslLogger = [self aslLoggerWritingToFileDescriptor:-1 withDebugLogging:debugLogging];
+  return [[FBControlCoreLogger_Composite alloc] initWithLoggers:@[
+    aslLogger,
+    [[FBControlCoreLogger_File alloc] initWithFileDescriptor:fileDescriptor],
+  ]];
+}
+
++ (FBControlCoreLogger_ASL *)aslLoggerWritingToFileDescriptor:(int)fileDescriptor withDebugLogging:(BOOL)debugLogging
+{
   FBASLClientManager *clientManager = [[FBASLClientManager alloc] initWithWritingToFileDescriptor:fileDescriptor debugLogging:debugLogging];
   asl_object_t client = [clientManager clientHandleForQueue:dispatch_get_main_queue()];
   FBControlCoreLogger_ASL *logger = [[FBControlCoreLogger_ASL alloc] initWithClientManager:clientManager client:client currentLevel:ASL_LEVEL_INFO prefix:nil];
@@ -179,3 +343,5 @@
 }
 
 @end
+
+#pragma clang diagnostic pop
