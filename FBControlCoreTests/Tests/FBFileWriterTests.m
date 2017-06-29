fd84d37615841418305a0d0476495b08b514fbb5
Lawrence Lomax <lawrencelomax@fb.com>
2017-06-29 07:20:31 -0700

    Do not write to a closed NSFileHandle in FBFileWriter
    Summary: Since calling `consumeEndOfFile` will close the file handle, we should nullify it to ensure that it can never be written to again.
    
    Reviewed By: zsol
    
    Differential Revision: D5301826
    
    fbshipit-source-id: e4945d991c5da2eaa6b3ba14818b40cf1039ed01

---
+++ FBControlCoreTests/Tests/FBFileWriterTests.m
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import <FBControlCore/FBControlCore.h>
+
+@interface FBFileWriterTests : XCTestCase
+
+@end
+
+@implementation FBFileWriterTests
+
+- (void)testClosesFileHandle
+{
+  // Setup
+  NSPipe *pipe = NSPipe.pipe;
+  FBFileWriter *writer = [FBFileWriter writerWithFileHandle:pipe.fileHandleForWriting blocking:YES];
+
+  // Write some data and confirm that it is as expected.
+  NSData *expected = [@"Foo Bar Baz" dataUsingEncoding:NSUTF8StringEncoding];
+  [writer consumeData:expected];
+  NSData *actual = [pipe.fileHandleForReading availableData];
+  XCTAssertEqualObjects(expected, actual);
+
+  // Close the handle, confirm it does not assert if more data arrives.
+  [writer consumeEndOfFile];
+  XCTAssertNoThrow([writer consumeData:expected]);
+
+  // There should be no more data to consume.
+  actual = [pipe.fileHandleForReading readDataToEndOfFile];
+  XCTAssertEqual(actual.length, 0u);
+}
+
+@end
