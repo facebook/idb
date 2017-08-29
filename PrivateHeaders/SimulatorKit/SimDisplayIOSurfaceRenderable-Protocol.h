1662b3ac622c3f60764c0c175cbd6758b5825f28
Lawrence Lomax <lawrencelomax@fb.com>
2017-08-29 11:53:55 -0700

    Document new SimulatorKit behaviour
    Summary: How clients are notified of changes to an Simulator's Framebuffer has changed. Updating documentation to make this clear.
    
    Reviewed By: perryjrandall
    
    Differential Revision: D5727846
    
    fbshipit-source-id: d378080869fd705cd9a4c47f6b36f8624e263f79

--- PrivateHeaders/SimulatorKit/SimDisplayIOSurfaceRenderable-Protocol.h
+++ PrivateHeaders/SimulatorKit/SimDisplayIOSurfaceRenderable-Protocol.h
@@ -11,13 +11,25 @@
 
 #import <SimulatorKit/FoundationXPCProtocolProxyable-Protocol.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @protocol SimDisplayIOSurfaceRenderable <FoundationXPCProtocolProxyable>
 
 /**
  In Xcode 8, this is an xpc_object_t
  In Xcode 9, this is an IOSurfaceRef.
+
+ If the host is running macOS 10.12 or greater an IOSurfaceRef is toll-free-bridged to an IOSurface *object*.
+ This is an Objective-C wrapper to the CoreFoundation IOSurfaceRef.
+
  Consumers should take this into account.
  */
 @property (readonly, nullable, nonatomic) id ioSurface;
 
+// Added in Xcode 9 as -[SimDeviceIOClient attachConsumer:] methods have been removed.
+- (void)unregisterIOSurfaceChangeCallbackWithUUID:(NSUUID *)arg1;
+- (void)registerCallbackWithUUID:(NSUUID *)arg1 ioSurfaceChangeCallback:(void (^)(id))arg2;
+
 @end
+
+NS_ASSUME_NONNULL_END
