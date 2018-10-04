e5eda35d05f81070d59fa59aba0de3ff01325233
Yingliu Chen <yingliu@fb.com>
2018-10-04 09:20:02 -0700

    Stop using Simulator.app to launch simulator
    Summary:
    There are some stability issue with Simulator.app in Xcode 10.0,
    so stop using it for now.
    
    Reviewed By: lawrencelomax
    
    Differential Revision: D10200401
    
    fbshipit-source-id: 7c8e38bb8c831d76e1fe66ef193b94ef7e6e2be3

--- fbxctest/FBXCTestKit/Runners/FBXCTestSimulatorFetcher.m
+++ fbxctest/FBXCTestKit/Runners/FBXCTestSimulatorFetcher.m
@@ -82,21 +82,9 @@
 
 - (FBFuture<FBSimulator *> *)fetchSimulatorForApplicationTest:(FBXCTestDestinationiPhoneSimulator *)destination
 {
-  FBSimulatorBootOptions options = FBSimulatorBootOptionsEnableDirectLaunch | FBSimulatorBootOptionsVerifyUsable;
-
-  if (FBXcodeConfiguration.isXcode10OrGreater) {
-    NSMutableDictionary<NSString *, NSString *> *environment = [NSProcessInfo.processInfo.environment mutableCopy];
-    NSString *videoRecordingPath = environment[@"FBXCTEST_VIDEO_RECORDING_PATH"];
-    if (videoRecordingPath != nil) {
-      // rdar://44907260
-      // In Xcode 10, we need to launch Simulator.app to setup simulator properly for enabling video recording.
-      options &= ~FBSimulatorBootOptionsEnableDirectLaunch;
-    }
-  }
-
   FBSimulatorBootConfiguration *bootConfiguration = [[FBSimulatorBootConfiguration
     defaultConfiguration]
-    withOptions:options];
+    withOptions:FBSimulatorBootOptionsEnableDirectLaunch | FBSimulatorBootOptionsVerifyUsable];
 
   return [[self
     fetchSimulatorForLogicTest:destination]
