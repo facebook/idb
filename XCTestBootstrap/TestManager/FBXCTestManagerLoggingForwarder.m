d43a0c4ad6684328f28380b5956ff1c7d9e5a017
Lawrence Lomax <lawrencelomax@fb.com>
2018-10-24 07:55:12 -0700

    Improve Logging
    Summary: Adds better logging here for when things go bad.
    
    Reviewed By: c-ryan747
    
    Differential Revision: D10527163
    
    fbshipit-source-id: 32799859e6c31e5c59b9f68ec85a6afd1391e6b3

--- XCTestBootstrap/TestManager/FBXCTestManagerLoggingForwarder.m
+++ XCTestBootstrap/TestManager/FBXCTestManagerLoggingForwarder.m
@@ -23,6 +23,8 @@
 
 @implementation FBXCTestManagerLoggingForwarder
 
+#pragma mark Initializers
+
 + (instancetype)withIDEInterface:(id<XCTestManager_IDEInterface, NSObject>)interface logger:(id<FBControlCoreLogger>)logger
 {
   return [[FBXCTestManagerLoggingForwarder alloc] initWithIDEInterface:interface logger:logger];
@@ -41,7 +43,45 @@
   return self;
 }
 
-#pragma mark Delegate Forwarding
+#pragma mark Default Implementations
+
+- (id)_XCT_testSuite:(NSString *)arg1 didStartAt:(NSString *)arg2
+{
+  [self.logger logFormat:@"Test Suite %@ started", arg1];
+  return [self.interface _XCT_testSuite:arg1 didStartAt:arg2];
+}
+
+- (id)_XCT_testSuite:(NSString *)arg1 didFinishAt:(NSString *)arg2 runCount:(NSNumber *)arg3 withFailures:(NSNumber *)arg4 unexpected:(NSNumber *)arg5 testDuration:(NSNumber *)arg6 totalDuration:(NSNumber *)arg7
+{
+  [self.logger logFormat:@"Test Suite Did Finish %@", arg1];
+  return [self.interface _XCT_testSuite:arg1 didFinishAt:arg2 runCount:arg3 withFailures:arg4 unexpected:arg5 testDuration:arg6 totalDuration:arg7];
+}
+
+- (id)_XCT_testCaseDidStartForTestClass:(NSString *)arg1 method:(NSString *)arg2
+{
+  [self.logger logFormat:@"Test Case %@/%@ did start", arg1, arg2];
+  return [self.interface _XCT_testCaseDidStartForTestClass:arg1 method:arg2];
+}
+
+- (id)_XCT_testCaseDidFinishForTestClass:(NSString *)arg1 method:(NSString *)arg2 withStatus:(NSString *)arg3 duration:(NSNumber *)arg4
+{
+  [self.logger logFormat:@"Test Case %@/%@ did finish (%@)", arg1, arg2, arg3];
+  return [self.interface _XCT_testCaseDidFinishForTestClass:arg1 method:arg2 withStatus:arg3 duration:arg4];
+}
+
+- (id)_XCT_testCaseDidFailForTestClass:(NSString *)arg1 method:(NSString *)arg2 withMessage:(NSString *)arg3 file:(NSString *)arg4 line:(NSNumber *)arg5
+{
+  [self.logger logFormat:@"Test Case %@/%@ did fail: %@", arg1, arg2, arg3];
+  return [self.interface _XCT_testCaseDidFailForTestClass:arg1 method:arg2 withMessage:arg3 file:arg4 line:arg5];
+}
+
+- (id)_XCT_logDebugMessage:(NSString *)arg1
+{
+  [self.logger log:[arg1 stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet]];
+  return [self.interface _XCT_logDebugMessage:arg1];
+}
+
+#pragma mark Forwarding Un-implemented selectors.
 
 - (BOOL)respondsToSelector:(SEL)selector
 {
@@ -61,13 +101,6 @@
   } else {
     [super forwardInvocation:invocation];
   }
-}
-
-#pragma mark Blackholing Some Logging
-
-- (id)_XCT_logDebugMessage:(NSString *)arg1
-{
-  return [self.interface _XCT_logDebugMessage:arg1];
 }
 
 @end
