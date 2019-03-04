7090f23b89dad7d5671aa7ae3f493d43278265e2
Lawrence Lomax <lawrencelomax@fb.com>
2019-03-04 01:58:39 -0800

    Non-modular header import in FBiOSTargetFuture
    Summary: This should be a non-modular import of the header
    
    Reviewed By: c-ryan747
    
    Differential Revision: D14261972
    
    fbshipit-source-id: cbd5df9effae69ee1e1f2c5dbfb1768823cfb4a6

--- FBControlCore/Actions/FBiOSTargetFuture.m
+++ FBControlCore/Actions/FBiOSTargetFuture.m
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#import <FBControlCore/FBiOSTargetFuture.h>
+#import "FBiOSTargetFuture.h"
 
 #import <objc/runtime.h>
 
