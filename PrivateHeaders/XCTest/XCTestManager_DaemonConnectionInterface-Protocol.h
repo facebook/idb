ff8691e4d4235a98f4a24f18074783a42943b710
Lawrence Lomax <lawrencelomax@fb.com>
2019-03-08 01:07:49 -0800

    Re-license as MIT
    Summary: $TITLE
    
    Reviewed By: zeyadsalloum, JoelMarcey
    
    Differential Revision: D14365624
    
    fbshipit-source-id: 803f4b69f8df5c45ac02ded04a4fd8f61bd502d7

--- PrivateHeaders/XCTest/XCTestManager_DaemonConnectionInterface-Protocol.h
+++ PrivateHeaders/XCTest/XCTestManager_DaemonConnectionInterface-Protocol.h
@@ -1,19 +1,8 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 @class NSArray, NSDictionary, NSNumber, NSString, NSUUID, DTXRemoteInvocationReceipt;
