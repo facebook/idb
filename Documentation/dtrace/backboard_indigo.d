ba67810217a9f4a34988b155f2dbec11c8aaac2b
Lawrence Lomax <lawrencelomax@fb.com>
2019-03-11 01:30:37 -0700

    License Header in SimulatorBridge
    Summary: #compliance
    
    Reviewed By: zeyadsalloum
    
    Differential Revision: D14385531
    
    fbshipit-source-id: 5760c1e1c82d8eb619ff48e2ec95cdc80650c028

--- Documentation/dtrace/backboard_indigo.d
+++ Documentation/dtrace/backboard_indigo.d
@@ -1,4 +1,10 @@
 /* vim: set tabstop=2 shiftwidth=2 filetype=dtrace: */
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 /* Tracing of Indigo Messages in backboardd */
 
 #include <SimulatorApp/Mach.h>
