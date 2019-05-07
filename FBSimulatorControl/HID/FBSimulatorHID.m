49f067904701f97dde753f3bf720c3c6957ba3ee
Brandon Evans <brandon@brandonevans.ca>
2019-05-07 04:00:01 -0700

    Return correct registration port value in Future-based API (#480)
    Summary:
    Hi! Most of the HID implementation is still beyond me, but as I work through trying to use it I noticed what looked like a small error made when this code was rewritten to use FBFutures in 21735b26b394d05f9b2da3c5d4f3235450cc5126.
    Pull Request resolved: https://github.com/facebook/idb/pull/480
    
    Differential Revision: D15239217
    
    Pulled By: lawrencelomax
    
    fbshipit-source-id: 1cf38746f846310d777002fb8c142db577fc0e3b

--- FBSimulatorControl/HID/FBSimulatorHID.m
+++ FBSimulatorControl/HID/FBSimulatorHID.m
@@ -142,7 +142,7 @@ static const char *SimulatorHIDClientClassName = "SimulatorKit.SimDeviceLegacyHI
         causedBy:innerError]
         failFuture];
     }
-    return [FBFuture futureWithResult:@(result)];
+    return [FBFuture futureWithResult:@(registrationPort)];
   }];
 }
 
