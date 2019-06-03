829dab7b6198945c37d2405871045d1e9070cd17
Callum Ryan <callumryan@fb.com>
2019-06-03 05:32:19 -0700

    Fix describe return value
    Reviewed By: lawrencelomax
    
    Differential Revision: D15600885
    
    fbshipit-source-id: 41aaa862a268fb33331f58115916b31aca989b95

--- idb/ipc/describe.py
+++ idb/ipc/describe.py
@@ -2,9 +2,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 
+from idb.common.types import TargetDescription
 from idb.grpc.idb_pb2 import TargetDescriptionRequest, TargetDescriptionResponse
 from idb.grpc.types import CompanionClient
+from idb.ipc.mapping.target import target_to_py
 
 
-async def client(client: CompanionClient) -> TargetDescriptionResponse:
-    return await client.stub.describe(TargetDescriptionRequest())
+async def client(client: CompanionClient) -> TargetDescription:
+    response = await client.stub.describe(TargetDescriptionRequest())
+    return target_to_py(response.target_description)
