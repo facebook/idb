96fc9cbf424dcf34d2ed44d58bc507c28e27a514
Lawrence Lomax <lawrencelomax@fb.com>
2019-06-17 05:06:24 -0700

    Add FBBinaryDescriptorTests
    Summary: With a refactor happening here, this helps
    
    Reviewed By: zeyadsalloum
    
    Differential Revision: D15851352
    
    fbshipit-source-id: 87ae7cdfc34efce5a7e7b69e8ffb75b6efdfbba6

---
+++ FBControlCoreTests/Tests/Unit/FBBinaryDescriptorTests.m
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import <FBControlCore/FBControlCore.h>
+
+@interface FBBinaryDescriptorTests : XCTestCase
+
+@end
+
+@implementation FBBinaryDescriptorTests
+
+- (void)testFatBinary
+{
+  // xctest is a fat binary.
+  NSError *error = nil;
+  FBBinaryDescriptor *descriptor = [FBBinaryDescriptor binaryWithPath:NSProcessInfo.processInfo.arguments.firstObject error:&error];
+  XCTAssertNil(error);
+  XCTAssertNotNil(descriptor);
+}
+
+- (void)test64BitMacosCommand
+{
+  // codesign is not a fat binary.
+  NSError *error = nil;
+  FBBinaryDescriptor *descriptor = [FBBinaryDescriptor binaryWithPath:@"/usr/bin/codesign" error:&error];
+  XCTAssertNil(error);
+  XCTAssertNotNil(descriptor);
+}
+
+@end
