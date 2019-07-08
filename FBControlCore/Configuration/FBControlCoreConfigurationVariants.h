8b4cd5d26110945094d8a34ba73c95c9827d9b80
Maxim Lobanov <v-malob@microsoft.com>
2019-07-08 13:16:46 +0300

    update devices

--- FBControlCore/Configuration/FBControlCoreConfigurationVariants.h
+++ FBControlCore/Configuration/FBControlCoreConfigurationVariants.h
@@ -107,6 +107,7 @@ extern FBOSVersionName const FBOSVersionNameiOS_11_4;
 extern FBOSVersionName const FBOSVersionNameiOS_12_0;
 extern FBOSVersionName const FBOSVersionNameiOS_12_1;
 extern FBOSVersionName const FBOSVersionNameiOS_12_2;
+extern FBOSVersionName const FBOSVersionNameiOS_13_0;
 extern FBOSVersionName const FBOSVersionNametvOS_9_0;
 extern FBOSVersionName const FBOSVersionNametvOS_9_1;
 extern FBOSVersionName const FBOSVersionNametvOS_9_2;
@@ -120,6 +121,8 @@ extern FBOSVersionName const FBOSVersionNametvOS_11_3;
 extern FBOSVersionName const FBOSVersionNametvOS_11_4;
 extern FBOSVersionName const FBOSVersionNametvOS_12_0;
 extern FBOSVersionName const FBOSVersionNametvOS_12_1;
+extern FBOSVersionName const FBOSVersionNametvOS_12_2;
+extern FBOSVersionName const FBOSVersionNametvOS_13_0;
 extern FBOSVersionName const FBOSVersionNamewatchOS_2_0;
 extern FBOSVersionName const FBOSVersionNamewatchOS_2_1;
 extern FBOSVersionName const FBOSVersionNamewatchOS_2_2;
@@ -131,6 +134,8 @@ extern FBOSVersionName const FBOSVersionNamewatchOS_4_1;
 extern FBOSVersionName const FBOSVersionNamewatchOS_4_2;
 extern FBOSVersionName const FBOSVersionNamewatchOS_5_0;
 extern FBOSVersionName const FBOSVersionNamewatchOS_5_1;
+extern FBOSVersionName const FBOSVersionNamewatchOS_5_2;
+extern FBOSVersionName const FBOSVersionNamewatchOS_6_0;
 
 #pragma mark Devices
 
