e5ae9a593f6d5fb6fd0b3acb4a70b10e8f04247c
Sylvain Pace <sylvain.pace@algolia.com>
2020-04-11 14:13:31 +0200

    feat(website): add DocSearch as recommended by docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,10 +6,6 @@
  */
 
 module.exports = {
-  algolia: {
-    apiKey: '0908032af0955efe731222e1de3bad86',
-    indexName: 'idb'
-  },
   title: 'idb',
   tagline: 'iOS Development Bridge',
   favicon: 'img/favicon.png',
@@ -18,6 +14,10 @@ module.exports = {
   organizationName: 'facebook',
   projectName: 'idb',
   themeConfig: {
+    algolia: {
+      apiKey: '0908032af0955efe731222e1de3bad86',
+      indexName: 'idb'
+    },
     navbar: {
       title: 'idb',
       links: [
