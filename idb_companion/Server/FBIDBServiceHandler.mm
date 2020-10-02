0b28654dc0885d7cd73ad3fd8ca6f5928a7ab356
Lawrence Lomax <lawrencelomax@fb.com>
2020-10-02 08:12:31 -0700

    Remove handling of legacy bundle_id in companion
    Summary:
    This was needed where old clients might not yet be sending FileContainer.
    
    This is now gone, so we can fully remove this.
    
    Reviewed By: c-ryan747
    
    Differential Revision: D24078330
    
    fbshipit-source-id: 0999a4801e022da357be40ebe02e8ec0e01dc973

--- idb_companion/Server/FBIDBServiceHandler.mm
+++ idb_companion/Server/FBIDBServiceHandler.mm
@@ -424,11 +424,9 @@ static idb::TargetDescription description_of_target(id<FBiOSTarget> target)
   return description;
 }
 
-static NSString *file_container(idb::FileContainer container, std::string bundleID)
+static NSString *file_container(idb::FileContainer container)
 {
   switch (container.kind()) {
-    case idb::FileContainer_Kind_APPLICATION:
-      return nsstring_from_c_string(container.bundle_id());
     case idb::FileContainer_Kind_ROOT:
       return FBFileContainerKindRoot;
     case idb::FileContainer_Kind_MEDIA:
@@ -437,14 +435,11 @@ static NSString *file_container(idb::FileContainer container, std::string bundle
       return FBFileContainerKindCrashes;
     case idb::FileContainer_Kind_PROVISIONING_PROFILES:
       return FBFileContainerKindProvisioningProfiles;
+    case idb::FileContainer_Kind_APPLICATION:
     default:
+      return nsstring_from_c_string(container.bundle_id());
       break;
   }
-  // Compatability cases, where we derive purely from the bundleID
-  if (bundleID.length() == 0) {
-    return nil;
-  }
-  return nsstring_from_c_string(bundleID);
 }
 
 static void populate_companion_info(idb::CompanionInfo *info, id<FBEventReporter> reporter, id<FBiOSTarget> target)
@@ -659,7 +654,7 @@ Status FBIDBServiceHandler::uninstall(ServerContext *context, const idb::Uninsta
 Status FBIDBServiceHandler::mkdir(grpc::ServerContext *context, const idb::MkdirRequest *request, idb::MkdirResponse *response)
 {@autoreleasepool{
   NSError *error = nil;
-  [[_commandExecutor create_directory:nsstring_from_c_string(request->path()) containerType:file_container(request->container(), request->bundle_id())] block:&error];
+  [[_commandExecutor create_directory:nsstring_from_c_string(request->path()) containerType:file_container(request->container())] block:&error];
   if (error) {
     return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
   }
@@ -673,7 +668,7 @@ Status FBIDBServiceHandler::mv(grpc::ServerContext *context, const idb::MvReques
   for (int j = 0; j < request->src_paths_size(); j++) {
     [originalPaths addObject:nsstring_from_c_string(request->src_paths(j))];
   }
-  [[_commandExecutor move_paths:originalPaths to_path:nsstring_from_c_string(request->dst_path()) containerType:file_container(request->container(), request->bundle_id())] block:&error];
+  [[_commandExecutor move_paths:originalPaths to_path:nsstring_from_c_string(request->dst_path()) containerType:file_container(request->container())] block:&error];
   if (error) {
     return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
   }
@@ -687,7 +682,7 @@ Status FBIDBServiceHandler::rm(grpc::ServerContext *context, const idb::RmReques
   for (int j = 0; j < request->paths_size(); j++) {
     [paths addObject:nsstring_from_c_string(request->paths(j))];
   }
-  [[_commandExecutor remove_paths:paths containerType:file_container(request->container(), request->bundle_id())] block:&error];
+  [[_commandExecutor remove_paths:paths containerType:file_container(request->container())] block:&error];
   if (error) {
     return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
   }
@@ -697,7 +692,7 @@ Status FBIDBServiceHandler::rm(grpc::ServerContext *context, const idb::RmReques
 Status FBIDBServiceHandler::ls(grpc::ServerContext *context, const idb::LsRequest *request, idb::LsResponse *response)
 {@autoreleasepool{
   NSError *error = nil;
-  NSArray<NSString *> *paths = [[_commandExecutor list_path:nsstring_from_c_string(request->path()) containerType:file_container(request->container(), request->bundle_id())] block:&error];
+  NSArray<NSString *> *paths = [[_commandExecutor list_path:nsstring_from_c_string(request->path()) containerType:file_container(request->container())] block:&error];
   if (error) {
     return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
   }
@@ -1053,7 +1048,7 @@ Status FBIDBServiceHandler::push(grpc::ServerContext *context, grpc::ServerReade
   const idb::PushRequest_Inner inner = request.inner();
 
   [[filepaths_from_reader(_commandExecutor.temporaryDirectory, reader, false, _target.logger) onQueue:_target.asyncQueue pop:^FBFuture<NSNull *> *(NSArray<NSURL *> *files) {
-    return [_commandExecutor push_files:files to_path:nsstring_from_c_string(inner.dst_path()) containerType:file_container(inner.container(), inner.bundle_id())];
+    return [_commandExecutor push_files:files to_path:nsstring_from_c_string(inner.dst_path()) containerType:file_container(inner.container())];
   }] block:&error];
   if (error) {
     return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
@@ -1066,7 +1061,7 @@ Status FBIDBServiceHandler::pull(ServerContext *context, const ::idb::PullReques
   NSString *path = nsstring_from_c_string(request->src_path());
   NSError *error = nil;
   if (request->dst_path().length() > 0) {
-    NSString *filePath = [[_commandExecutor pull_file_path:path destination_path:nsstring_from_c_string(request->dst_path()) containerType:file_container(request->container(), request->bundle_id()) ] block:&error];
+    NSString *filePath = [[_commandExecutor pull_file_path:path destination_path:nsstring_from_c_string(request->dst_path()) containerType:file_container(request->container()) ] block:&error];
     if (error) {
       return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
     }
@@ -1074,7 +1069,7 @@ Status FBIDBServiceHandler::pull(ServerContext *context, const ::idb::PullReques
   } else {
     NSURL *url = [_commandExecutor.temporaryDirectory temporaryDirectory];
     NSString *tempPath = [url.path stringByAppendingPathComponent:path.lastPathComponent];
-    NSString *filePath = [[_commandExecutor pull_file_path:path destination_path:tempPath containerType:file_container(request->container(), request->bundle_id())] block:&error];
+    NSString *filePath = [[_commandExecutor pull_file_path:path destination_path:tempPath containerType:file_container(request->container())] block:&error];
     if (error) {
       return Status(grpc::StatusCode::INTERNAL, error.localizedDescription.UTF8String);
     }
