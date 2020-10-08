3cb4a89e8f66ce37db03ff188dec3caa42101b70
Lawrence Lomax <lawrencelomax@fb.com>
2020-10-08 02:44:15 -0700

    Remove client side sending of bundle_id
    Summary: This is now no longer present in the companion handling, so let's remove it from the proto
    
    Reviewed By: c-ryan747
    
    Differential Revision: D24078439
    
    fbshipit-source-id: c480222df39a7232b2793862ac6b59bd9b6ee0bc

--- idb/grpc/client.py
+++ idb/grpc/client.py
@@ -79,10 +79,7 @@ from idb.grpc.crash import (
     _to_crash_log_info_list,
     _to_crash_log_query_proto,
 )
-from idb.grpc.file import (
-    container_to_bundle_id_deprecated as file_container_to_bundle_id_deprecated,
-    container_to_grpc as file_container_to_grpc,
-)
+from idb.grpc.file import container_to_grpc as file_container_to_grpc
 from idb.grpc.hid import event_to_grpc
 from idb.grpc.idb_grpc import CompanionServiceStub
 from idb.grpc.idb_pb2 import (
@@ -474,11 +471,7 @@ class IdbClient(IdbClientBase):
     @log_and_handle_exceptions
     async def rm(self, container: FileContainer, paths: List[str]) -> None:
         await self.stub.rm(
-            RmRequest(
-                bundle_id=file_container_to_bundle_id_deprecated(container),
-                paths=paths,
-                container=file_container_to_grpc(container),
-            )
+            RmRequest(paths=paths, container=file_container_to_grpc(container))
         )
 
     @log_and_handle_exceptions
@@ -487,7 +480,6 @@ class IdbClient(IdbClientBase):
     ) -> None:
         await self.stub.mv(
             MvRequest(
-                bundle_id=file_container_to_bundle_id_deprecated(container),
                 src_paths=src_paths,
                 dst_path=dest_path,
                 container=file_container_to_grpc(container),
@@ -499,11 +491,7 @@ class IdbClient(IdbClientBase):
         self, container: FileContainer, path: str
     ) -> List[FileEntryInfo]:
         response = await self.stub.ls(
-            LsRequest(
-                bundle_id=file_container_to_bundle_id_deprecated(container),
-                path=path,
-                container=file_container_to_grpc(container),
-            )
+            LsRequest(path=path, container=file_container_to_grpc(container))
         )
         return [FileEntryInfo(path=file.path) for file in response.files]
 
@@ -523,11 +511,7 @@ class IdbClient(IdbClientBase):
     @log_and_handle_exceptions
     async def mkdir(self, container: FileContainer, path: str) -> None:
         await self.stub.mkdir(
-            MkdirRequest(
-                bundle_id=file_container_to_bundle_id_deprecated(container),
-                path=path,
-                container=file_container_to_grpc(container),
-            )
+            MkdirRequest(path=path, container=file_container_to_grpc(container))
         )
 
     @log_and_handle_exceptions
@@ -590,9 +574,7 @@ class IdbClient(IdbClientBase):
             await stream.send_message(
                 PushRequest(
                     inner=PushRequest.Inner(
-                        bundle_id=file_container_to_bundle_id_deprecated(container),
-                        dst_path=dest_path,
-                        container=file_container_to_grpc(container),
+                        dst_path=dest_path, container=file_container_to_grpc(container)
                     )
                 )
             )
@@ -619,7 +601,6 @@ class IdbClient(IdbClientBase):
     ) -> None:
         async with self.stub.pull.open() as stream:
             request = request = PullRequest(
-                bundle_id=file_container_to_bundle_id_deprecated(container),
                 src_path=src_path,
                 # not sending the destination to remote companion
                 # so it streams the file back
--- idb/grpc/file.py
+++ idb/grpc/file.py
@@ -10,23 +10,13 @@ from idb.grpc.idb_pb2 import FileContainer as GrpcFileContainer
 
 def container_to_grpc(container: FileContainer) -> GrpcFileContainer:
     if isinstance(container, str):
-        return GrpcFileContainer(
-            kind=GrpcFileContainer.APPLICATION, bundle_id=container
-        )
+        return GrpcFileContainer(kind=GrpcFileContainer.APPLICATION)
     if container == FileContainerType.MEDIA:
-        return GrpcFileContainer(kind=GrpcFileContainer.MEDIA, bundle_id=None)
+        return GrpcFileContainer(kind=GrpcFileContainer.MEDIA)
     if container == FileContainerType.CRASHES:
-        return GrpcFileContainer(kind=GrpcFileContainer.CRASHES, bundle_id=None)
+        return GrpcFileContainer(kind=GrpcFileContainer.CRASHES)
     if container == FileContainerType.ROOT:
-        return GrpcFileContainer(kind=GrpcFileContainer.ROOT, bundle_id=None)
+        return GrpcFileContainer(kind=GrpcFileContainer.ROOT)
     if container == FileContainerType.PROVISIONING_PROFILES:
-        return GrpcFileContainer(
-            kind=GrpcFileContainer.PROVISIONING_PROFILES, bundle_id=None
-        )
-    return GrpcFileContainer(kind=GrpcFileContainer.NONE, bundle_id=None)
-
-
-def container_to_bundle_id_deprecated(container: FileContainer) -> str:
-    if isinstance(container, str):
-        return container
-    return ""
+        return GrpcFileContainer(kind=GrpcFileContainer.PROVISIONING_PROFILES)
+    return GrpcFileContainer(kind=GrpcFileContainer.NONE)
