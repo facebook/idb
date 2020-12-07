08d40699470b8f7d2572f69cea39cfc0913e5b3a
Lawrence Lomax <lawrencelomax@fb.com>
2020-12-07 04:27:35 -0800

    Add docs for FBDeviceControl
    Summary: Detailing some of the intricacies here.
    
    Reviewed By: zeyadsalloum
    
    Differential Revision: D25333952
    
    fbshipit-source-id: 473cbd01b1a2ad671af160d32cbe9da605d048e9

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,9 +1,10 @@
-/*
+/**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
  
 module.exports = {
   "docs": [
@@ -11,6 +12,7 @@ module.exports = {
     "installation",
     "guided-tour",
     "architecture",
+    "fbdevicecontrol",
     "commands",
     "test-execution",
     "accessibility",
