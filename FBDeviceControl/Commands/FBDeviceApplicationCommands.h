552dbedb2f3ee184a0ad85cabe0f9ed14e514f5a
Andreas Reich <andreasr@unity3d.com>
2020-12-17 20:00:56 +0100

    Documented now exposed deltaInstallApplicatoinWithPath

--- FBDeviceControl/Commands/FBDeviceApplicationCommands.h
+++ FBDeviceControl/Commands/FBDeviceApplicationCommands.h
@@ -24,6 +24,14 @@ NS_ASSUME_NONNULL_BEGIN
  @return a new instance of the Command.
  */
 + (instancetype)commandsWithTarget:(FBDevice *)target;
+
+/**
+ Installs application at given path on the host using a shadow dir on the host to only install changed files.
+
+ @param path the file path of the Application Bundle on the host.
+ @param shadowDir directory on the host that is used for the shadow copy.
+ @return A future that resolves when successful.
+ */
 - (FBFuture<NSNull *> *)deltaInstallApplicationWithPath:(NSString *)path andShadowDirectory:(NSString *)shadowDir;
 
 @end
