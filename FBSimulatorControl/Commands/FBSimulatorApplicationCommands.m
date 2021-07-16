2a7263f72957fe7eacc7c164eae7b5a718b8eaf8
Lawrence Lomax <lawrencelomax@fb.com>
2021-07-16 01:54:09 -0700

    Flatten FBSimulatorApplicationLaunchStrategy into FBSimulatorApplicationCommands
    Summary: This is needless indirection now that this strategy class is only used from `FBSimulatorApplicationCommands`
    
    Reviewed By: jamescoggan
    
    Differential Revision: D29694663
    
    fbshipit-source-id: a5c8911aea5d2611df409a6b76bed7d389d7ab87

--- FBSimulatorControl/Commands/FBSimulatorApplicationCommands.m
+++ FBSimulatorControl/Commands/FBSimulatorApplicationCommands.m
@@ -13,10 +13,10 @@
 
 #import "FBSimulator+Private.h"
 #import "FBSimulator.h"
-#import "FBSimulatorApplicationLaunchStrategy.h"
 #import "FBSimulatorError.h"
 #import "FBSimulatorLaunchCtlCommands.h"
 #import "FBSimulatorLaunchedApplication.h"
+#import "FBSimulatorProcessLaunchStrategy.h"
 #import "FBSimulatorSubprocessTerminationStrategy.h"
 
 @interface FBSimulatorApplicationCommands ()
@@ -63,9 +63,19 @@
 
 - (FBFuture<FBSimulatorLaunchedApplication *> *)launchApplication:(FBApplicationLaunchConfiguration *)configuration
 {
-  return [[FBSimulatorApplicationLaunchStrategy
-    strategyWithSimulator:self.simulator]
-    launchApplication:configuration];
+  FBSimulator *simulator = self.simulator;
+  FBProcessIO *io = configuration.io;
+  return [[[FBFuture futureWithFutures:@[
+      [self ensureApplicationIsInstalled:configuration.bundleID],
+      [self confirmApplicationLaunchState:configuration.bundleID launchMode:configuration.launchMode waitForDebugger:configuration.waitForDebugger],
+    ]]
+    onQueue:simulator.workQueue fmap:^(id _) {
+      return [io attachViaFile];
+    }]
+    onQueue:simulator.workQueue fmap:^ FBFuture<FBSimulatorLaunchedApplication *> * (FBProcessFileAttachment *attachment) {
+      FBFuture<NSNumber *> *launch = [self launchApplication:configuration stdOut:attachment.stdOut stdErr:attachment.stdErr];
+      return [FBSimulatorLaunchedApplication applicationWithSimulator:simulator configuration:configuration attachment:attachment launchFuture:launch];
+    }];
 }
 
 - (FBFuture<NSNull *> *)killApplicationWithBundleID:(NSString *)bundleID
@@ -176,6 +186,119 @@
 }
 
 #pragma mark Private
+
+- (FBFuture<NSNumber *> *)ensureApplicationIsInstalled:(NSString *)bundleID
+{
+  return [[[self.simulator
+    installedApplicationWithBundleID:bundleID]
+    mapReplace:NSNull.null]
+    onQueue:self.simulator.asyncQueue handleError:^(NSError *error) {
+      return [[FBSimulatorError
+        describeFormat:@"App %@ can't be launched as it isn't installed: %@", bundleID, error]
+        failFuture];
+    }];
+}
+
+- (FBFuture<NSNumber *> *)confirmApplicationLaunchState:(NSString *)bundleID launchMode:(FBApplicationLaunchMode)launchMode waitForDebugger:(BOOL)waitForDebugger
+{
+  if (waitForDebugger && launchMode == FBApplicationLaunchModeForegroundIfRunning) {
+    return [[FBSimulatorError
+      describe:@"'Foreground if running' and 'wait for debugger cannot be applied simultaneously"]
+      failFuture];
+  }
+
+  FBSimulator *simulator = self.simulator;
+  return [[simulator
+    processIDWithBundleID:bundleID]
+    onQueue:simulator.asyncQueue chain:^ FBFuture<NSNull *> * (FBFuture<NSNumber *> *processFuture) {
+      NSNumber *processID = processFuture.result;
+      if (!processID) {
+        return FBFuture.empty;
+      }
+      if (launchMode == FBApplicationLaunchModeFailIfRunning) {
+        return [[FBSimulatorError
+          describeFormat:@"App %@ can't be launched as is running (PID=%@)", bundleID, processID]
+          failFuture];
+      } else if (launchMode == FBApplicationLaunchModeRelaunchIfRunning) {
+        return [[FBSimulatorSubprocessTerminationStrategy
+          strategyWithSimulator:simulator]
+          terminateApplication:bundleID];
+      }
+      return FBFuture.empty;
+  }];
+}
+
+- (FBFuture<NSNumber *> *)launchApplication:(FBApplicationLaunchConfiguration *)configuration stdOut:(id<FBProcessFileOutput>)stdOut stdErr:(id<FBProcessFileOutput>)stdErr
+{
+  // Start reading now, but don't block on the resolution, we will ensure that the read has started after the app has launched.
+  FBFuture *readingFutures = [FBFuture futureWithFutures:@[
+    [stdOut startReading],
+    [stdErr startReading],
+  ]];
+
+  return [[self
+    launchApplication:configuration stdOutPath:stdOut.filePath stdErrPath:stdErr.filePath]
+    onQueue:self.simulator.workQueue fmap:^(NSNumber *result) {
+      return [readingFutures mapReplace:result];
+    }];
+}
+
+- (FBFuture<NSNumber *> *)isApplicationRunning:(NSString *)bundleID
+{
+  return [[self.simulator
+    processIDWithBundleID:bundleID]
+    onQueue:self.simulator.workQueue chain:^(FBFuture<NSNumber *> *future){
+      NSNumber *processIdentifier = future.result;
+      return processIdentifier ? [FBFuture futureWithResult:@YES] : [FBFuture futureWithResult:@NO];
+    }];
+}
+
+- (FBFuture<NSNumber *> *)launchApplication:(FBApplicationLaunchConfiguration *)configuration stdOutPath:(NSString *)stdOutPath stdErrPath:(NSString *)stdErrPath
+{
+  FBSimulator *simulator = self.simulator;
+  NSDictionary<NSString *, id> *options = [FBSimulatorApplicationCommands
+    simDeviceLaunchOptionsForConfiguration:configuration
+    stdOutPath:[self translateAbsolutePath:stdOutPath toPathRelativeTo:simulator.dataDirectory]
+    stdErrPath:[self translateAbsolutePath:stdErrPath toPathRelativeTo:simulator.dataDirectory]];
+
+  FBMutableFuture<NSNumber *> *future = [FBMutableFuture future];
+  [simulator.device launchApplicationAsyncWithID:configuration.bundleID options:options completionQueue:simulator.workQueue completionHandler:^(NSError *error, pid_t pid){
+    if (error) {
+      [future resolveWithError:error];
+    } else {
+      [future resolveWithResult:@(pid)];
+    }
+  }];
+  return future;
+}
+
+- (NSString *)translateAbsolutePath:(NSString *)absolutePath toPathRelativeTo:(NSString *)referencePath
+{
+  if (![absolutePath hasPrefix:@"/"]) {
+    return absolutePath;
+  }
+  // When launching an application with a custom stdout/stderr path, `SimDevice` uses the given path relative
+  // to the Simulator's data directory. From the Framework's consumer point of view this might not be the
+  // wanted behaviour. To work around it, we construct a path relative to the Simulator's data directory
+  // using `..` until we end up in the absolute path outside the Simulator's data directory.
+  NSString *translatedPath = @"";
+  for (NSUInteger index = 0; index < referencePath.pathComponents.count; index++) {
+    translatedPath = [translatedPath stringByAppendingPathComponent:@".."];
+  }
+  return [translatedPath stringByAppendingPathComponent:absolutePath];
+}
+
++ (NSDictionary<NSString *, id> *)simDeviceLaunchOptionsForConfiguration:(FBApplicationLaunchConfiguration *)configuration stdOutPath:(nullable NSString *)stdOutPath stdErrPath:(nullable NSString *)stdErrPath
+{
+  NSMutableDictionary<NSString *, id> *options = [[FBSimulatorProcessLaunchStrategy launchOptionsWithArguments:configuration.arguments environment:configuration.environment waitForDebugger:configuration.waitForDebugger] mutableCopy];
+  if (stdOutPath){
+    options[@"stdout"] = stdOutPath;
+  }
+  if (stdErrPath) {
+    options[@"stderr"] = stdErrPath;
+  }
+  return [options copy];
+}
 
 static NSString *const KeyDataContainer = @"DataContainer";
 
