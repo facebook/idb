91035cd21e96c9d01b06b280555b50d1f615d95a
Lawrence Lomax <lawrencelomax@fb.com>
2021-08-13 04:30:26 -0700

    Remove FBiOSTargetDefaultVideoPath + FBiOSTargetDefaultScreenshotPath
    Summary: No longer used
    
    Reviewed By: jamescoggan
    
    Differential Revision: D30301344
    
    fbshipit-source-id: 37664537666f000a168d000d4fe918a0eb702889

--- FBControlCore/Management/FBiOSTarget.m
+++ FBControlCore/Management/FBiOSTarget.m
@@ -132,13 +132,3 @@ NSComparisonResult FBiOSTargetComparison(id<FBiOSTarget> left, id<FBiOSTarget> r
   }
   return [left.udid compare:right.udid];
 }
-
-NSString *FBiOSTargetDefaultScreenshotPath(NSString *storageDirectory)
-{
-  return [storageDirectory stringByAppendingPathComponent:@"video.mp4"];
-}
-
-NSString *FBiOSTargetDefaultVideoPath(NSString *storageDirectory)
-{
-  return [storageDirectory stringByAppendingPathComponent:@"screenshot.png"];
-}
