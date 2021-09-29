d049b68672c8f408d541b8eaad39ad6d75b8ba08
Johnny Souza <johnnysouza@fb.com>
2021-09-29 02:40:50 -0700

    only look for libs folders if sanitiser libs aren't required
    Summary: as title
    
    Reviewed By: jamescoggan
    
    Differential Revision: D31235546
    
    fbshipit-source-id: 2da4ba09377e36b2ab6209c20a56efaae23873b4

--- XCTestBootstrap/Utility/FBOToolDynamicLibs.m
+++ XCTestBootstrap/Utility/FBOToolDynamicLibs.m
@@ -12,20 +12,22 @@
 @implementation FBOToolDynamicLibs
 
 + (FBFuture<NSArray *> *)findFullPathForSanitiserDyldInBundle:(NSString *)bundlePath onQueue:(nonnull dispatch_queue_t)queue {
-    NSString *clanLocation = [FBXcodeConfiguration.developerDirectory stringByAppendingPathComponent:@"Toolchains/XcodeDefault.xctoolchain/usr/lib/clang"];
-    NSError *error = nil;
-    NSArray<NSURL *> *fileList = [self filesInDirectory:[NSURL fileURLWithPath:clanLocation] error:&error];
-    
-    if ([fileList count] == 0 || error) {
-        if(error == nil) return [[FBControlCoreError
-                                  describeFormat:@"No clang version found in %@", clanLocation] failFuture];
-        return [FBFuture futureWithError:error];
-    }
-    
-    NSString *libsFolder = [NSString pathWithComponents: @[[fileList[0] path], @"lib/darwin/"]];
     
     return [[FBOToolOperation listSanitiserDylibsRequiredByBundle:bundlePath onQueue:queue] onQueue:queue map:^id _Nonnull(NSArray<NSString *> * _Nonnull libsList) {
-        NSMutableArray *libraries = [[NSMutableArray alloc]init];
+      
+        NSString *clanLocation = [FBXcodeConfiguration.developerDirectory stringByAppendingPathComponent:@"Toolchains/XcodeDefault.xctoolchain/usr/lib/clang"];
+        NSError *error = nil;
+        NSArray<NSURL *> *fileList = [self filesInDirectory:[NSURL fileURLWithPath:clanLocation] error:&error];
+        
+        if ([fileList count] == 0 || error) {
+            if(error == nil) return [[FBControlCoreError
+                                      describeFormat:@"No clang version found in %@", clanLocation] failFuture];
+            return [FBFuture futureWithError:error];
+        }
+        
+        NSString *libsFolder = [NSString pathWithComponents: @[[fileList[0] path], @"lib/darwin/"]];
+
+        NSMutableArray *libraries = [[NSMutableArray alloc] init];
         for (NSString* lib in libsList) {
             NSString *libPath = [libsFolder stringByAppendingPathComponent:lib];
             [libraries addObject:libPath];
