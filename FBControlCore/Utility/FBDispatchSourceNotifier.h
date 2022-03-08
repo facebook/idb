70337797cda26bf882c5b7aa2ddc336be5af928a
Lawrence Lomax <lawrencelomax@fb.com>
2022-03-08 03:37:36 -0800

    Remove unused dispatch_source for timer
    Summary:
    This has been fully replaced with other API usage so can be removed.
    
    The implementation of the class itself can also be removed since this dependency is now gone also. This just leaves `processTerminationFutureNotifierForProcessIdentifier` which has one caller and will be moved in the next diff
    
    Reviewed By: jamescoggan
    
    Differential Revision: D34687560
    
    fbshipit-source-id: 87b9ca724555a00cda8bf7bfec251b6d6ddfb146

--- FBControlCore/Utility/FBDispatchSourceNotifier.h
+++ FBControlCore/Utility/FBDispatchSourceNotifier.h
@@ -26,29 +26,6 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (FBFuture<NSNumber *> *)processTerminationFutureNotifierForProcessIdentifier:(pid_t)processIdentifier;
 
-/**
- Creates and returns an `FBDispatchSourceNotifier` that will call the `handler` at a provided timing interval.
-
- @param timeInterval the time interval to wait for.
- @param queue the queue to call back on.
- @param handler the handler to call when the process exits
- */
-+ (instancetype)timerNotifierNotifierWithTimeInterval:(uint64_t)timeInterval queue:(dispatch_queue_t)queue handler:(void (^)(FBDispatchSourceNotifier *))handler;
-
-#pragma mark Public Methods
-
-/**
- Stops the Notifier.
- */
-- (void)terminate;
-
-#pragma mark Properties
-
-/**
- The Wrapped Dispatch Source.
- */
-@property (nonatomic, strong, nullable, readonly) dispatch_source_t dispatchSource;
-
 @end
 
 NS_ASSUME_NONNULL_END
