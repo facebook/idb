306ed2c8c903058b577d06b27da3d34c121ea298
Nikita Patskov <patskovn@fb.com>
2022-04-19 04:11:51 -0700

    Add documentation
    Summary: As in title
    
    Reviewed By: jbardini
    
    Differential Revision: D35496902
    
    fbshipit-source-id: bdc48f5a85ed2aa8cd82ce3d69d4deac45858667

--- idb_companion/Utility/BridgeFuture.swift
+++ idb_companion/Utility/BridgeFuture.swift
@@ -9,19 +9,29 @@ import Foundation
 import FBControlCore
 
 enum FBFutureError: Error {
+
+  /// This indicates an error in objc code where result callback was called but no error or result provided. In case of this, debug `FBFuture` implementation.
   case continuationFullfilledWithoutValues
+
+  /// This indicates an error in `BridgeFuture.values` implementation. In case of this, debug `BridgeFuture.values` implementation.
   case taskGroupReceivedNilResultInternalError
 }
 
 /// Swift compiler does not allow usage of generic parameters of objc classes in extension
-/// so we need to create a bridge class
+/// so we need to create a bridge for convenience.
 enum BridgeFuture {
 
+  /// Use this to receive results from multiple futures. The results are **ordered in the same order as passed futures**, so you can safely access them from
+  /// array by indexes.
+  /// - Note: We should *not* use @discardableResult, results should be dropped explicitly by the callee.
   static func values<T: AnyObject>(_ futures: FBFuture<T>...) async throws -> [T] {
     let futuresArr: [FBFuture<T>] = futures
     return try await values(futuresArr)
   }
 
+  /// Use this to receive results from multiple futures. The results are **ordered in the same order as passed futures**, so you can safely access them from
+  /// array by indexes.
+  /// - Note: We should *not* use @discardableResult, results should be dropped explicitly by the callee.
   static func values<T: AnyObject>(_ futures: [FBFuture<T>]) async throws -> [T] {
     return try await withThrowingTaskGroup(of: (Int, T).self, returning: [T].self) { group in
       var results = [T?].init(repeating: nil, count: futures.count)
@@ -47,6 +57,7 @@ enum BridgeFuture {
   }
 
   /// Awaitable value that waits for publishing from the wrapped future
+  /// - Note: We should *not* use @discardableResult, results should be dropped explicitly by the callee.
   static func value<T: AnyObject>(_ future: FBFuture<T>) async throws -> T {
     try await withTaskCancellationHandler {
       try await withCheckedThrowingContinuation { continuation in
@@ -65,15 +76,20 @@ enum BridgeFuture {
     }
   }
 
+  /// NSNull is Void equivalent in objc reference world. So is is safe to ignore the result.
   static func await(_ future: FBFuture<NSNull>) async throws {
     _ = try await Self.value(future)
   }
 
+  /// This overload exists because of `FBMutableFuture` does not convert its exact generic type automatically but it can be automatically converted to `FBFuture<AnyObject>`
+  /// without any problems. This decision may be revisited in future.
   static func await(_ future: FBFuture<AnyObject>) async throws {
     _ = try await Self.value(future)
   }
 
-  /// Interop between swift and objc generics are quite bad, so we have to write wrappers like this
+  /// Interop between swift and objc generics are quite bad, so we have to write wrappers like this.
+  /// By default swift bridge compiler could not convert generic type of `FBMutableFuture`. But this force cast is 100% valid and works in runtime
+  /// so we just use this little helper.
   static func convertToFuture<T: AnyObject>(_ mutableFuture: FBMutableFuture<T>) -> FBFuture<T> {
     let future: FBFuture<AnyObject> = mutableFuture
     return future as! FBFuture<T>
