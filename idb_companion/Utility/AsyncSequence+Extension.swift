27c98a645b98ad31696da1f61c2073cf2ae0b1dd
Nikita Patskov <patskovn@fb.com>
2022-04-19 04:11:51 -0700

    Intoduce requiredNext to async stream
    Summary:
    We have quite a lot of grpc request streams where we read request N constant number of times and do not need `foreach` loop. But pure `next` producec optinal by design.
    This small tweak just saves us from lots of boilerplate of unwrapping the optionals everywhere
    
    Reviewed By: jbardini
    
    Differential Revision: D35496906
    
    fbshipit-source-id: 9748895b90817ffdbba502d73d2c1ea6927fc0f0

--- idb_companion/Utility/AsyncSequence+Extension.swift
+++ idb_companion/Utility/AsyncSequence+Extension.swift
@@ -6,12 +6,35 @@
  */
 
 import Foundation
+import GRPC
+
+enum StreamReadError<Element>: Error, GRPCStatusTransformable {
+  case nextElementNotProduced
+
+  func makeGRPCStatus() -> GRPCStatus {
+    switch self {
+    case .nextElementNotProduced:
+      return GRPCStatus(code: .failedPrecondition, message: "Expected next element of type \(Element.self)")
+    }
+  }
+}
 
 extension AsyncSequence {
 
   var next: Element? {
     get async throws {
       return try await first(where: { _ in true })
+    }
+  }
+
+  /// We have quite a lot of grpc request streams where we read request N constant number of times and do not need foreach loop. But pure next produces optinal by design.
+  /// This small tweak just saves us from lots of boilerplate of unwrapping the optionals everywhere
+  var requiredNext: Element {
+    get async throws {
+      guard let next = try await first(where: { _ in true }) else {
+        throw StreamReadError<Element>.nextElementNotProduced
+      }
+      return next
     }
   }
 
