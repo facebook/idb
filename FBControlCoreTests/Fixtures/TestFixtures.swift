c006484fc9ea2ecbbd4d2a491014179c7e41866c
Nikita Patskov <patskovn@fb.com>
2022-04-27 04:07:31 -0700

    Migrate FBCrashLogInfoTests to swift
    Summary: It is much leaner and easier to write swift tests. And it require no effort in bridging, because swift can import testable modules without headers. So it is time
    
    Reviewed By: jbardini
    
    Differential Revision: D35812411
    
    fbshipit-source-id: f30d3cb9f337c460f6051dcaa0f7a09ba32ebbdc

---
+++ FBControlCoreTests/Fixtures/TestFixtures.swift
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+
+private class BundleFinder {}
+
+enum TestFixtures {
+
+  static let assetsdCrashPathWithCustomDeviceSet = Bundle(for: BundleFinder.self)
+    .path(forResource: "assetsd_custom_set", ofType: "crash")!
+
+  static let appCrashPathWithDefaultDeviceSet = Bundle(for: BundleFinder.self)
+    .path(forResource: "app_default_set", ofType: "crash")!
+
+  static let appCrashPathWithCustomDeviceSet = Bundle(for: BundleFinder.self)
+    .path(forResource: "app_custom_set", ofType: "crash")!
+
+  static let agentCrashPathWithCustomDeviceSet = Bundle(for: BundleFinder.self)
+    .path(forResource: "agent_custom_set", ofType: "crash")!
+}
