c1eaafa44be53719fa93ed4ea52660bfa8e003b5
Nikita Patskov <patskovn@fb.com>
2022-06-20 07:02:00 -0700

    Hold strong reference on sim set in FBSimulator
    Summary:
    In release buiulds `weak` references actually start to work and `FBSimulatorSet` dies when all strong references on it disappears.
    As long as `FBSimulatorSet` is a singleton, it is okay to have retain cycle.
    
    Reviewed By: lawrencelomax
    
    Differential Revision: D37238837
    
    fbshipit-source-id: 6085dfe1fcae60bf2e28c22c3f04a66b2e3362e7

--- FBSimulatorControl/Management/FBSimulator.h
+++ FBSimulatorControl/Management/FBSimulator.h
@@ -42,8 +42,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  The Simulator Set that the Simulator belongs to.
+ Reference to `FBSimulatorSet` results to a strong-strong reference cycle between `FBSimulatorSet` and `FBSimulator`.
+ However, this cycle is explicitly broken by `FBSimulatorSet` when a `FBSimulator` is removed from the set that `FBSimulatorSet` wraps.
  */
-@property (nonatomic, weak, readonly, nullable) FBSimulatorSet *set;
+@property (nonatomic, strong, readonly, nonnull) FBSimulatorSet *set;
 
 /**
  The Product Family of the Simulator.
