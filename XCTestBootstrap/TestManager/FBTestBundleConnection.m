99f82069450c61b8122fb3e01c1c0739dbf848d3
Nikita Patskov <patskovn@meta.com>
2023-01-09 05:55:36 -0800

    Increased intermediate timeouts for connecting to the resource bundle
    Summary: Silly me forgot to also increase intermediate timeouts
    
    Reviewed By: fgasperij
    
    Differential Revision: D42414787
    
    fbshipit-source-id: 71d501cdd5c855b65bfd6d1e27f275c41038d432

--- XCTestBootstrap/TestManager/FBTestBundleConnection.m
+++ XCTestBootstrap/TestManager/FBTestBundleConnection.m
@@ -26,8 +26,8 @@
 #import "FBTestManagerAPIMediator.h"
 
 static NSTimeInterval BundleReadyTimeout = 60; // Time for `_XCT_testBundleReadyWithProtocolVersion` to be called after the 'connect'.
-static NSTimeInterval IDEInterfaceReadyTimeout = 10; // Time for `XCTestManager_IDEInterface` to be returned.
-static NSTimeInterval DaemonSessionReadyTimeout = 10; // Time for `_IDE_initiateSessionWithIdentifier` to be returned.
+static NSTimeInterval IDEInterfaceReadyTimeout = 30; // Time for `XCTestManager_IDEInterface` to be returned.
+static NSTimeInterval DaemonSessionReadyTimeout = 30; // Time for `_IDE_initiateSessionWithIdentifier` to be returned.
 static NSTimeInterval CrashCheckWaitLimit = 30;  // Time to wait for crash report to be generated.
 
 #pragma clang diagnostic push
