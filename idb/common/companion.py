f608ea3d86469b8d6b7a7770db9dd117d0cb50a1
Jacek Olczyk <joald@meta.com>
2024-02-29 07:53:33 -0800

    Back out "Add support for idb-targets to iOS RM"
    Summary:
    Original commit changeset: f8080c5d20ba
    
    Original Phabricator Diff: D52659078
    
    Differential Revision: D54359346
    
    fbshipit-source-id: 72fe6fdde646f89209b35d5ab16c9eecc61352b1

--- idb/common/companion.py
+++ idb/common/companion.py
@@ -50,7 +50,7 @@ class CompanionSpawnerException(Exception):
     pass
 
 
-async def terminate_process(
+async def _terminate_process(
     process: asyncio.subprocess.Process, timeout: timedelta, logger: logging.Logger
 ) -> None:
     returncode = process.returncode
@@ -198,7 +198,7 @@ class Companion(CompanionBase):
         try:
             yield process
         finally:
-            await terminate_process(
+            await _terminate_process(
                 process=process,
                 timeout=DEFAULT_COMPANION_TEARDOWN_TIMEOUT,
                 logger=logger,
