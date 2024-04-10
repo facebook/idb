f77cff74777f93487a8ceb7c66231d949d2fafe0
Nikita Patskov <patskovn@meta.com>
2024-04-10 12:35:48 -0700

    sed died -> exited
    Reviewed By: jbardini
    
    Differential Revision: D55980816
    
    fbshipit-source-id: d9e16e337e0ce3016c524c45028f7edd82659b77

--- FBControlCore/Commands/FBProcessSpawnCommands.m
+++ FBControlCore/Commands/FBProcessSpawnCommands.m
@@ -60,14 +60,14 @@
       [statLocFuture resolveWithResult:@(statLoc)];
       if (WIFSIGNALED(statLoc)) {
         int signalCode = WTERMSIG(statLoc);
-        NSString *message = [NSString stringWithFormat:@"Process %d (%@) died with signal %d", processIdentifier, configuration.processName, signalCode];
+        NSString *message = [NSString stringWithFormat:@"Process %d (%@) exited with signal %d", processIdentifier, configuration.processName, signalCode];
         [logger log:message];
         NSError *error = [[FBControlCoreError describe:message] build];
         [exitCodeFuture resolveWithError:error];
         [signalFuture resolveWithResult:@(signalCode)];
       } else {
         int exitCode = WEXITSTATUS(statLoc);
-        NSString *message = [NSString stringWithFormat:@"Process %d (%@) died with exit code %d", processIdentifier, configuration.processName, exitCode];
+        NSString *message = [NSString stringWithFormat:@"Process %d (%@) exited with code %d", processIdentifier, configuration.processName, exitCode];
         [logger log:message];
         NSError *error = [[FBControlCoreError describe:message] build];
         [signalFuture resolveWithError:error];
