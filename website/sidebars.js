fc16aad18226ca300aa71c1c6065f024f40e418d
Gerrit Goossen <gerritg@meta.com>
2026-04-10 13:43:29 -0700

    Move website source directory
    Reviewed By: bryanyantx
    
    Differential Revision: D100339666
    
    fbshipit-source-id: 9c7aa4875d487e23a6bac9d23848bae3f3d6b30a

---
+++ website/sidebars.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ 
+module.exports = {
+  "docs": [
+    "overview",
+    "installation",
+    "guided-tour",
+    "architecture",
+    "development",
+    "commands",
+    "fbsimulatorcontrol",
+    "fbdevicecontrol",
+    "video",
+    "test-execution",
+    "file-containers",
+    "accessibility",
+    "faqs"
+  ]
+};
