8706a0350d7b6d2a37a929ba3303182313c5972a
Lawrence Lomax <lawrencelomax@meta.com>
2026-04-13 06:24:58 -0700

    Remove bridge fallback from location override
    Summary: Remove the `FBSimulatorBridge` fallback path from `overrideLocationWithLongitude:latitude:`. The `SimDevice.setLocationWithLatitude:andLongitude:error:` API has been available since Xcode 12. Also use `resolveValue:` pattern.
    
    Reviewed By: xgerrit
    
    Differential Revision: D99292021
    
    fbshipit-source-id: 7469f058ae3038e5d1a24aff390a461456320a8a

--- FBSimulatorControl/Commands/FBSimulatorLocationCommands.m
+++ FBSimulatorControl/Commands/FBSimulatorLocationCommands.m
@@ -9,7 +9,6 @@
 #import "FBSimulatorLocationCommands.h"
 
 #import "FBSimulator.h"
-#import "FBSimulatorBridge.h"
 
 @interface FBSimulatorLocationCommands ()
 
@@ -42,23 +41,12 @@
 
 - (FBFuture<NSNull *> *)overrideLocationWithLongitude:(double)longitude latitude:(double)latitude
 {
-  if ([self.simulator.device respondsToSelector:(@selector(setLocationWithLatitude:andLongitude:error:))]) {
-    return [FBFuture onQueue:self.simulator.workQueue resolve:^ FBFuture<NSNull *> * () {
-      NSError *error = nil;
-      BOOL succeeded = [self.simulator.device setLocationWithLatitude:latitude andLongitude:longitude error:&error];
-      if (!succeeded) {
-        return [FBFuture futureWithError:error];
-      }
-      
-      return FBFuture.empty;
-    }];
-  }
-  
-  return [[self.simulator
-    connectToBridge]
-    onQueue:self.simulator.workQueue fmap:^ FBFuture<NSNull *> * (FBSimulatorBridge *bridge) {
-      return [bridge setLocationWithLatitude:latitude longitude:longitude];
-    }];
+  return [FBFuture onQueue:self.simulator.workQueue resolveValue:^NSNull *(NSError **error) {
+    if (![self.simulator.device setLocationWithLatitude:latitude andLongitude:longitude error:error]) {
+      return nil;
+    }
+    return NSNull.null;
+  }];
 }
 
 @end
