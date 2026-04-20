81a7875f0895944cecbbddc0a67592b4c3e51e66
Lawrence Lomax <lawrencelomax@meta.com>
2026-04-20 05:26:38 -0700

    Add unit tests for ProxyService dict builders
    Summary:
    Tests the pure CoreFoundation dictionary construction logic in ProxyService.
    
    - `testBuildHTTPProxyDictContainsHTTPKeys`: verifies HTTPProxy, HTTPPort, HTTPEnable
    - `testBuildHTTPProxyDictContainsHTTPSKeys`: verifies HTTPSProxy, HTTPSPort, HTTPSEnable
    - `testBuildHTTPProxyDictContainsFTPPassiveAndExceptions`: verifies FTPPassive and ExceptionsList
    - `testBuildSOCKSProxyDictContainsSOCKSKeys`: verifies SOCKSProxy, SOCKSPort, SOCKSEnable
    - `testBuildSOCKSProxyDictDoesNotContainHTTPKeys`: verifies no HTTP keys leak into SOCKS dict
    - `testBuildEmptyProxyDictContainsOnlyFTPPassive`: verifies exactly 1 key (FTPPassive=1)
    
    Differential Revision: D100958282
    
    fbshipit-source-id: 94d996336df72c0412c9bf395272b884986caf7b

--- SimulatorFrameworkBridgeLibTests/ProxyServiceTests.m
+++ SimulatorFrameworkBridgeLibTests/ProxyServiceTests.m
@@ -31,4 +31,103 @@
   XCTAssertEqual(handleProxyAction(@"set", @[@"127.0.0.1"]), 1);
 }
 
+#pragma mark - buildHTTPProxyDict
+
+- (void)testBuildHTTPProxyDictContainsHTTPKeys
+{
+  CFMutableDictionaryRef dict = buildHTTPProxyDict(@"10.0.0.1", 8080);
+  XCTAssertNotEqual(dict, NULL);
+
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+  XCTAssertEqualObjects(ns[@"HTTPProxy"], @"10.0.0.1");
+  XCTAssertEqualObjects(ns[@"HTTPPort"], @8080);
+  XCTAssertEqualObjects(ns[@"HTTPEnable"], @1);
+
+  CFRelease(dict);
+}
+
+- (void)testBuildHTTPProxyDictContainsHTTPSKeys
+{
+  CFMutableDictionaryRef dict = buildHTTPProxyDict(@"proxy.example.com", 3128);
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+
+  XCTAssertEqualObjects(ns[@"HTTPSProxy"], @"proxy.example.com");
+  XCTAssertEqualObjects(ns[@"HTTPSPort"], @3128);
+  XCTAssertEqualObjects(ns[@"HTTPSEnable"], @1);
+
+  CFRelease(dict);
+}
+
+- (void)testBuildHTTPProxyDictContainsFTPPassiveAndExceptions
+{
+  CFMutableDictionaryRef dict = buildHTTPProxyDict(@"127.0.0.1", 8080);
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+
+  XCTAssertEqualObjects(ns[@"FTPPassive"], @1);
+  NSArray *exceptions = ns[@"ExceptionsList"];
+  XCTAssertEqual(exceptions.count, 2u);
+  XCTAssertTrue([exceptions containsObject:@"*.local"]);
+  XCTAssertTrue([exceptions containsObject:@"169.254/16"]);
+
+  CFRelease(dict);
+}
+
+#pragma mark - buildSOCKSProxyDict
+
+- (void)testBuildSOCKSProxyDictContainsSOCKSKeys
+{
+  CFMutableDictionaryRef dict = buildSOCKSProxyDict(@"10.0.0.1", 1080);
+  XCTAssertNotEqual(dict, NULL);
+
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+  XCTAssertEqualObjects(ns[@"SOCKSProxy"], @"10.0.0.1");
+  XCTAssertEqualObjects(ns[@"SOCKSPort"], @1080);
+  XCTAssertEqualObjects(ns[@"SOCKSEnable"], @1);
+
+  CFRelease(dict);
+}
+
+- (void)testBuildSOCKSProxyDictDoesNotContainHTTPKeys
+{
+  CFMutableDictionaryRef dict = buildSOCKSProxyDict(@"10.0.0.1", 1080);
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+
+  XCTAssertNil(ns[@"HTTPProxy"]);
+  XCTAssertNil(ns[@"HTTPPort"]);
+  XCTAssertNil(ns[@"HTTPEnable"]);
+  XCTAssertNil(ns[@"HTTPSProxy"]);
+  XCTAssertNil(ns[@"HTTPSPort"]);
+  XCTAssertNil(ns[@"HTTPSEnable"]);
+
+  CFRelease(dict);
+}
+
+- (void)testBuildSOCKSProxyDictContainsFTPPassiveAndExceptions
+{
+  CFMutableDictionaryRef dict = buildSOCKSProxyDict(@"10.0.0.1", 1080);
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+
+  XCTAssertEqualObjects(ns[@"FTPPassive"], @1);
+  NSArray *exceptions = ns[@"ExceptionsList"];
+  XCTAssertEqual(exceptions.count, 2u);
+  XCTAssertTrue([exceptions containsObject:@"*.local"]);
+  XCTAssertTrue([exceptions containsObject:@"169.254/16"]);
+
+  CFRelease(dict);
+}
+
+#pragma mark - buildEmptyProxyDict
+
+- (void)testBuildEmptyProxyDictContainsOnlyFTPPassive
+{
+  CFMutableDictionaryRef dict = buildEmptyProxyDict();
+  XCTAssertNotEqual(dict, NULL);
+
+  NSDictionary *ns = (__bridge NSDictionary *)dict;
+  XCTAssertEqual(ns.count, 1u);
+  XCTAssertEqualObjects(ns[@"FTPPassive"], @1);
+
+  CFRelease(dict);
+}
+
 @end
