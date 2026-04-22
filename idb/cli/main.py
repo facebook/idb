341db61cb32b5ced9bf4b1b9072adf3f6441371d
Gerrit Goossen <gerritg@meta.com>
2026-04-21 21:41:05 -0700

    Update the default path for `idb_companion`
    Summary:
    ### Context
    
    There was a recent change to prefer the chef installed version of `idb_companion` over the DotSlash version. The problem is that the path that's user for the local version is not the one installed by the `fb-idb` rpm.
    
    ### This diff
    
    Updates the local `idb_companion` path to the path installed by `fb-idb`.
    
    Reviewed By: bryanyantx
    
    Differential Revision: D101872216
    
    fbshipit-source-id: 83b994cdf7ce22fc150901cc9a0fc39eb4b17470

--- idb/cli/main.py
+++ idb/cli/main.py
@@ -124,7 +124,7 @@ def get_default_companion_path() -> str | None:
     # Prefer the direct binary over the wrapper script at /usr/local/bin/idb_companion,
     # which invokes a DotSlash stub that can fail due to environment differences
     # (e.g., XAR/PAR modifying PATH to include an incompatible dotslash binary).
-    direct_path = "/opt/facebook/bin/idb_companion"
+    direct_path = "/opt/facebook/idb/bin/idb_companion"
     if os.path.isfile(direct_path):
         return direct_path
     return shutil.which("idb_companion") or "/usr/local/bin/idb_companion"
