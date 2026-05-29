527ed7a0139c6403f254a00bf0ca7a64814b408e
generatedunixname1552096092572566 <noreply+1552096092572566@fb.com>
2026-05-29 13:04:21 -0700

    Apply iOS Swift idiomatic cleanup fbobjc/Tools/idb/Source/FBControlCore/Configuration/FBInstrumentsConfiguration.swift
    Reviewed By: xgerrit
    
    Differential Revision: D106779994
    
    fbshipit-source-id: c52280196f98d2a963315009e52a656f9eb09a7d

--- FBControlCore/Configuration/FBInstrumentsConfiguration.swift
+++ FBControlCore/Configuration/FBInstrumentsConfiguration.swift
@@ -17,7 +17,7 @@ public final class FBInstrumentsTimings: NSObject {
 
   @objc(timingsWithTerminateTimeout:launchRetryTimeout:launchErrorTimeout:operationDuration:)
   public class func timings(withTerminateTimeout terminateTimeout: TimeInterval, launchRetryTimeout: TimeInterval, launchErrorTimeout: TimeInterval, operationDuration: TimeInterval) -> FBInstrumentsTimings {
-    return FBInstrumentsTimings(terminateTimeout: terminateTimeout, launchRetryTimeout: launchRetryTimeout, launchErrorTimeout: launchErrorTimeout, operationDuration: operationDuration)
+    FBInstrumentsTimings(terminateTimeout: terminateTimeout, launchRetryTimeout: launchRetryTimeout, launchErrorTimeout: launchErrorTimeout, operationDuration: operationDuration)
   }
 
   @objc
@@ -42,7 +42,7 @@ public final class FBInstrumentsConfiguration: NSObject, NSCopying {
 
   @objc(configurationWithTemplateName:targetApplication:appEnvironment:appArguments:toolArguments:timings:)
   public class func configuration(withTemplateName templateName: String, targetApplication: String, appEnvironment: [String: String], appArguments: [String], toolArguments: [String], timings: FBInstrumentsTimings) -> FBInstrumentsConfiguration {
-    return FBInstrumentsConfiguration(templateName: templateName, targetApplication: targetApplication, appEnvironment: appEnvironment, appArguments: appArguments, toolArguments: toolArguments, timings: timings)
+    FBInstrumentsConfiguration(templateName: templateName, targetApplication: targetApplication, appEnvironment: appEnvironment, appArguments: appArguments, toolArguments: toolArguments, timings: timings)
   }
 
   @objc
@@ -57,10 +57,10 @@ public final class FBInstrumentsConfiguration: NSObject, NSCopying {
   }
 
   public override var description: String {
-    return "Instruments \(templateName) | \(targetApplication) | \(FBCollectionInformation.oneLineDescription(from: appEnvironment)) | \(FBCollectionInformation.oneLineDescription(from: appArguments)) | \(FBCollectionInformation.oneLineDescription(from: toolArguments)) | duration \(timings.operationDuration) | terminate timeout \(timings.terminateTimeout) | launch retry timeout \(timings.launchRetryTimeout) | launch error timeout \(timings.launchErrorTimeout)"
+    "Instruments \(templateName) | \(targetApplication) | \(FBCollectionInformation.oneLineDescription(from: appEnvironment)) | \(FBCollectionInformation.oneLineDescription(from: appArguments)) | \(FBCollectionInformation.oneLineDescription(from: toolArguments)) | duration \(timings.operationDuration) | terminate timeout \(timings.terminateTimeout) | launch retry timeout \(timings.launchRetryTimeout) | launch error timeout \(timings.launchErrorTimeout)"
   }
 
   public func copy(with zone: NSZone? = nil) -> Any {
-    return self
+    self
   }
 }
