0da6002c4fa8a91f0e0fdb44ecb014c06e89cb0d
Lawrence Lomax <lawrencelomax@meta.com>
2026-06-12 05:16:26 -0700

    Remove dead pre-Xcode-11 SimDeviceNotifier polyfill
    Summary: Remove the `registerNotificationHandler:` fallback from `FBCoreSimulatorNotifier` and the matching `optional` declaration from the `SimDeviceNotifier` private header. Apple removed that no-queue method in Xcode 11.0; the `respondsToSelector:` guard always selects `registerNotificationHandlerOnQueue:handler:` on supported Xcodes, so the fallback branch has been dead. No behavioral change.
    
    Reviewed By: xgerrit
    
    Differential Revision: D108270970
    
    fbshipit-source-id: 94ec3fb497e0f053832ad5e5f52acd0b77b8f74f

--- PrivateHeaders/CoreSimulator/SimDeviceNotifier-Protocol.h
+++ PrivateHeaders/CoreSimulator/SimDeviceNotifier-Protocol.h
@@ -12,9 +12,4 @@
 @protocol SimDeviceNotifier
 - (BOOL)unregisterNotificationHandler:(unsigned long long)arg1 error:(id *)arg2;
 - (unsigned long long)registerNotificationHandlerOnQueue:(NSObject<OS_dispatch_queue> *)arg1 handler:(void (^)(NSDictionary *))arg2;
-
-// Removed in Xcode 11.0
-@optional;
-- (unsigned long long)registerNotificationHandler:(void (^)(NSDictionary *))arg1;
-
 @end
