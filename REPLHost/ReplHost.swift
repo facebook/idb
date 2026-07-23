ac823d52daab4042596613f4b34bffb08c29b956
Gerrit Goossen <gerritg@meta.com>
2026-07-23 13:01:25 -0700

    Add the default ReplHost app
    Summary:
    ### This diff
    
    Adds an empty application to the `Resources/` that can be used as the default app for code injection in repl sessions.
    
    Reviewed By: bryanyantx
    
    Differential Revision: D113305085
    
    fbshipit-source-id: e3ee400dfeab10bbda459faaf51c87bac56e92c4

---
+++ REPLHost/ReplHost.swift
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import SwiftUI
+
+@main
+struct ReplHost: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
+    EmptyView()
+  }
+}
